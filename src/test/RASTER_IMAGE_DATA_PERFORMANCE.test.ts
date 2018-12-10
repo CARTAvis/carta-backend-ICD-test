2b9d7a7f9b92826c367d68bcb8bddd34ffe8d736
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2018-12-10 14:26:44 +0800

    Fix parameter source.

--- src/test/RASTER_IMAGE_DATA_PERFORMANCE.test.ts
+++ src/test/RASTER_IMAGE_DATA_PERFORMANCE.test.ts
@@ -154,8 +154,8 @@ describe("RASTER_IMAGE_DATA_PERFORMANCE tests", () => {
                                         // Preapare the message
                                         let messageSetImageView = CARTA.SetImageView.create({
                                             fileId: 0, imageBounds: {xMin: 0, xMax: openFileMessage.fileInfoExtended.width, yMin: 0, yMax: openFileMessage.fileInfoExtended.height}, 
-                                            mip: mip, compressionType: compressionType, 
-                                            compressionQuality: compressionQuality, numSubsets: numSubsets
+                                            mip, compressionType, 
+                                            compressionQuality, numSubsets
                                         });
                                         payload = CARTA.SetImageView.encode(messageSetImageView).finish();
                                         eventDataTx = new Uint8Array(32 + 4 + payload.byteLength);
