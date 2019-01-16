86eeb0c49ada8114806486a6b441d791cf2bedb7
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-01-16 12:40:39 +0800

    Refactor for issue #1.

--- src/test/ACCESS_CARTA_DEFAULT_CONCURRENT.test.ts
+++ src/test/ACCESS_CARTA_DEFAULT_CONCURRENT.test.ts
@@ -9,74 +9,107 @@ import * as Utility from "./testUtilityFunction";
 let testNumber = 10;
 let Connection: WebSocket[] = new Array(testNumber);
 
-describe("Access Websocket concurrent tests", () => {
+describe("Access Websocket concurrent test", () => {
     beforeAll( done => {
+        let promiseSet: Promise<void>[] = Array(testNumber);
+        
         for (let idx = 0; idx < testNumber; idx++) {
-            Connection[idx] = new WebSocket(testServerUrl);
+            promiseSet[idx] = new Promise( (resolve, reject) => {
+                Connection[idx] = new WebSocket(testServerUrl);
+                Connection[idx].onopen = () => {
+                    if (Connection[idx].readyState === WebSocket.OPEN) {
+                        resolve();
+                    } else {
+                        console.log(`connection #${idx + 1} can not open. @${new Date()}`);
+                        reject();
+                    }
+                }; 
+            });
         }
-        for (let idx = 0; idx < testNumber; idx++) {
-            Connection[idx].onopen = () => {
-                Connection[idx].close();
-                done();
-            }; 
-        }
+
+        Promise.all(promiseSet).then( () => done() );
     });
 
-    for (let idx = 0; idx < testNumber; idx++) {
-        test(`establish the connection #${idx + 1} to "${testServerUrl}".`, 
-        done => {
-            Connection[idx].onclose = () => {
-                done(); // Return to this test
-            };
-        }, connectTimeout);
-    }
+    test(`establish ${testNumber} connections to "${testServerUrl}".`, 
+    done => {
+        let promiseSet: Promise<void>[] = Array(testNumber);
+        
+        for (let idx = 0; idx < testNumber; idx++) {
+            promiseSet[idx] = new Promise( (resolve, reject) => {
+                Connection[idx].onclose = () => {
+                    resolve();
+                }; 
+            });
+        }
+
+        Promise.all(promiseSet).then( () => done() );
+
+        for (let idx = 0; idx < testNumber; idx++) {
+         Connection[idx].close();
+        }
+    }, connectTimeout);
+        
 });
 
-describe("ACCESS_CARTA_DEFAULT_CONCURRENT tests: Testing multiple concurrent connections to the backend.", () => {
+describe("ACCESS_CARTA_DEFAULT_CONCURRENT test: Testing multiple concurrent connections to the backend.", () => {
     
     beforeAll( done => {
+        let promiseSet: Promise<void>[] = Array(testNumber);
+        
         for (let idx = 0; idx < testNumber; idx++) {
-            Connection[idx] = new WebSocket(testServerUrl);
-            Connection[idx].binaryType = "arraybuffer";
+            promiseSet[idx] = new Promise( (resolve, reject) => {
+                Connection[idx] = new WebSocket(testServerUrl);
+                Connection[idx].binaryType = "arraybuffer";
+                Connection[idx].onopen = () => {
+                    if (Connection[idx].readyState === WebSocket.OPEN) {
+                        resolve();
+                    } else {
+                        console.log(`connection #${idx + 1} can not open. @${new Date()}`);
+                        reject();
+                    }
+                }; 
+            });
         }
-        for (let idx = 0; idx < testNumber; idx++) {
-            Connection[idx].onopen = () => {
-                // Checkout if Websocket server is ready
-                if (Connection[idx].readyState === WebSocket.OPEN) {
-                    // Preapare the message on a eventData
-                    let message = CARTA.RegisterViewer.create({sessionId: "", apiKey: "1234"});
-                    let payload = CARTA.RegisterViewer.encode(message).finish();
-                    let eventData = new Uint8Array(32 + 4 + payload.byteLength);
 
-                    eventData.set(Utility.stringToUint8Array("REGISTER_VIEWER", 32));
-                    eventData.set(new Uint8Array(new Uint32Array([1]).buffer), 32);
-                    eventData.set(payload, 36);
-
-                    Connection[idx].send(eventData);
-
-                    done();
-                } else {
-                    console.log(`connection #${idx + 1} can not open. @${new Date()}`);
-                }
-            }; 
-        }
+        Promise.all(promiseSet).then( () => done() );
     });
 
     let registerViewerAck: CARTA.RegisterViewerAck[] = new Array(testNumber);
-    for (let idx = 0; idx < testNumber; idx++) {
-        test(`connection #${idx + 1}: send EventName: "${"REGISTER_VIEWER"}" to CARTA "${testServerUrl}" with no session_id & api_key "1234".`, 
-        done => {
-            Connection[idx].onmessage = (event: MessageEvent) => {
-                expect(event.data.byteLength).toBeGreaterThan(40);
-                let eventName = Utility.getEventName(new Uint8Array(event.data, 0, 32));
-                if (eventName === "REGISTER_VIEWER_ACK") {
-                    let eventData = new Uint8Array(event.data, 36);
-                    registerViewerAck[idx] = CARTA.RegisterViewerAck.decode(eventData);
-                    done();
-                } // if
-            };
-        }, connectTimeout);
 
+    test(`${testNumber} connections send EventName: "REGISTER_VIEWER" to CARTA "${testServerUrl}" with no session_id & api_key "1234".`, 
+    done => {
+        let promiseSet: Promise<void>[] = Array(testNumber);
+        
+        for (let idx = 0; idx < testNumber; idx++) {
+            promiseSet[idx] = new Promise( (resolve, reject) => {
+                Connection[idx].onmessage = (event: MessageEvent) => {
+                    expect(event.data.byteLength).toBeGreaterThan(40);
+                    let eventName = Utility.getEventName(new Uint8Array(event.data, 0, 32));
+                    if (eventName === "REGISTER_VIEWER_ACK") {
+                        let eventData = new Uint8Array(event.data, 36);
+                        registerViewerAck[idx] = CARTA.RegisterViewerAck.decode(eventData);
+                        resolve();
+                    }
+                };
+            });
+        }
+
+        Promise.all(promiseSet).then( () => done() );
+
+        let messageRegisterViewer = CARTA.RegisterViewer.create({sessionId: "", apiKey: "1234"});
+        let payloadRegisterViewer = CARTA.RegisterViewer.encode(messageRegisterViewer).finish();
+        let eventDataRegisterViewer = new Uint8Array(32 + 4 + payloadRegisterViewer.byteLength);
+
+        eventDataRegisterViewer.set(Utility.stringToUint8Array("REGISTER_VIEWER", 32));
+        eventDataRegisterViewer.set(new Uint8Array(new Uint32Array([1]).buffer), 32);
+        eventDataRegisterViewer.set(payloadRegisterViewer, 36);
+
+        Connection.forEach( (item, index, array) => {
+            item.send(eventDataRegisterViewer);
+        });
+    }, connectTimeout);
+        
+    for (let idx = 0; idx < testNumber; idx++) {
         test(`connection #${idx + 1}: assert REGISTER_VIEWER_ACK.success to be True.`, 
         () => {
             expect(registerViewerAck[idx].success).toBe(true);
