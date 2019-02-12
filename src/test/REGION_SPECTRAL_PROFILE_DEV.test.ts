8a3b52aff7e4b45d661e9d34faaf88022d966fe2
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-02-12 17:14:41 +0800

    Add testing: REGION_SPECTRAL_PROFILE_DEV

---
+++ src/test/REGION_SPECTRAL_PROFILE_DEV.test.ts
@@ -0,0 +1,131 @@
+/// Manual
+let testServerUrl = "ws://127.0.0.1:1234";
+// let testServerUrl = "wss://acdc0.asiaa.sinica.edu.tw/socket2";
+let testSubdirectoryName = "set_QA";
+let connectionTimeout = 1000;
+let operationTimeout = 500;
+
+/// ICD defined
+import {CARTA} from "carta-protobuf";
+import * as Utility from "./testUtilityFunction";
+
+let testFileName = "S255_IR_sci.spw25.cube.I.pbcor.fits";
+
+describe("REGION_SPECTRAL_PROFILE_DEV: Temporary test case of region spectral profile to assist backend development", () => {   
+    let Connection: WebSocket;
+
+    beforeAll( done => {
+        // Establish a websocket connection in the binary form: arraybuffer 
+        Connection = new WebSocket(testServerUrl);
+        Connection.binaryType = "arraybuffer";
+        // While open a Websocket
+        Connection.onopen = () => {
+            // Checkout if Websocket server is ready
+            if (Connection.readyState === WebSocket.OPEN) {
+                
+                Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
+                    (RegisterViewerAck: CARTA.RegisterViewerAck) => {
+                        expect(RegisterViewerAck.success).toBe(true);
+                        
+                        Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
+                            (OpenFileAck: CARTA.OpenFileAck) => {
+                                expect(OpenFileAck.success).toBe(true);
+
+                                Utility.getEvent(Connection, "RASTER_IMAGE_DATA", CARTA.RasterImageData, 
+                                    (RasterImageData: CARTA.RasterImageData) => {
+                                        expect(RasterImageData.imageData.length).toBeGreaterThan(1);
+                                        
+                                        done();
+                                });  
+                                
+                                Utility.setEvent(Connection, "SET_IMAGE_VIEW", CARTA.SetImageView,
+                                    {
+                                        fileId: 0, 
+                                        imageBounds: {xMin: 0, xMax: 1920, yMin: 0, yMax: 1920}, 
+                                        mip: 4, 
+                                        compressionType: CARTA.CompressionType.ZFP, 
+                                        compressionQuality: 11, 
+                                        numSubsets: 4
+                                    }
+                                );
+                            }  
+                        );
+                        
+                        Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
+                        {
+                            directory: testSubdirectoryName, 
+                            file: testFileName, hdu: "0", fileId: 0, 
+                            renderMode: CARTA.RenderMode.RASTER
+                        }
+                        );
+
+                    }
+                );
+                
+                Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+                    {
+                        sessionId: "", 
+                        apiKey: "1234"
+                    }
+                );
+
+            } else {
+                console.log(`Can not open a connection. @${new Date()}`);
+                done();
+            }
+            
+        };
+    }, connectionTimeout);
+ 
+    test(`Test set region.`, 
+    done => {
+        Utility.getEvent(Connection, "SET_REGION_ACK", CARTA.SetRegionAck, 
+            (SetRegionAck: CARTA.SetRegionAck) => {
+                expect(SetRegionAck.success).toBe(true);
+                expect(SetRegionAck.regionId).toEqual(1);
+                
+                done();
+            }
+        );  
+
+        Utility.setEvent(Connection, "SET_REGION", CARTA.SetRegion, 
+            {
+                fileId: 0, regionId: -1, regionName: "a box",
+                regionType: CARTA.RegionType.RECTANGLE, 
+                channelMin: -1, channelMax: -1, stokes: [],
+                controlPoints: [{x: 1000, y: 1300}, {x: 200, y: 300}],
+                rotation: 0
+            }
+        );
+
+    }, operationTimeout);
+
+    test(`Test region spectral profile.`, 
+    done => {
+
+        Utility.getEvent(Connection, "SPECTRAL_PROFILE_DATA", CARTA.SpectralProfileData, 
+            (SpectralProfileData: CARTA.SpectralProfileData) => {
+                expect(SpectralProfileData.fileId).toEqual(0);
+                expect(SpectralProfileData.regionId).toEqual(1);
+                expect(SpectralProfileData.stokes).toEqual(0);
+
+                expect(SpectralProfileData.profiles[218]).toEqual(3.506743836071e-03);
+                
+                done();
+            }
+        ); 
+
+        Utility.setEvent(Connection, "SET_SPECTRAL_REQUIREMENTS", CARTA.SetSpectralRequirements, 
+            {
+                fileId: 0, regionId: 1, 
+                spectralProfiles: [{coordinate: "z", statsTypes: [CARTA.StatsType.Mean]}]
+            }
+        );
+        
+    }, operationTimeout);
+
+    afterAll( done => {
+        Connection.close();
+        done();
+    });
+});
