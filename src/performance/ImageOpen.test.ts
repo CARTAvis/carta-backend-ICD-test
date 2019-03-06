df50aa68a23470cebfd760b908e5e3b52d6530c4
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-03-06 11:36:33 +0800

    Updated

--- src/performance/ImageOpen.test.ts
+++ src/performance/ImageOpen.test.ts
@@ -5,19 +5,40 @@ import * as os from "os";
 
 let serverURL = "ws://127.0.0.1";
 let port = 5678;
-let directory = "/Users/zarda/GitHub/carta-backend-nrao/build";
+let backendDirectory = "/Users/zarda/GitHub/carta-backend-nrao/build";
+let baseDirectory = "$HOME/CARTA/Images";
+let testDirectory = "set_QA_performance";    
 let connectTimeout = 2000;
 let openFileTimeout = 8000;
 let logMessage = false;
+let imageFiles: string[] = [
+    "cube_A_03200_z00001.fits", 
+    "cube_B_03200_z00001.fits", 
+    "cube_C_03200_z00001.fits", 
+    "cube_D_03200_z00001.fits", 
+    "cube_E_03200_z00001.fits", 
+    "cube_F_03200_z00001.fits",
+];
+let imageIdx = -1;
+function arrayNext (arr: any) {
+    arr.next = () => { 
+        if (++imageIdx >= arr.length) {
+            imageIdx = 0;
+        } 
+        return arr[imageIdx];
+    };
+    arr.current = () => { return arr[imageIdx]; };
+    return arr;
+}
 
 describe("Image open performance: ", () => {    
  
     test(`Preparing... dry run.`, 
     done => {
         let cartaBackend = child_process.exec(
-            `"./carta_backend" folder=$HOME/CARTA/Images port=${port} threads=16`,
+            `"./carta_backend" root=base base=${baseDirectory} port=${port} threads=16`,
             {
-                cwd: directory, 
+                cwd: backendDirectory, 
                 timeout: 20000
             }
         );
@@ -37,14 +58,12 @@ describe("Image open performance: ", () => {
         Connection.binaryType = "arraybuffer";
         Connection.onopen = () => {
             Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
-                async RegisterViewerAck => {
+                RegisterViewerAck => {
                     expect(RegisterViewerAck.success).toBe(true);
-                    let cpuUsage0 = os.cpus()[0].times.user / os.cpus()[0].times  
                     
                     Connection.close();
                 }
             );
-
             Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
                 {
                     sessionId: "", 
@@ -61,10 +80,9 @@ describe("Image open performance: ", () => {
         
     }, connectTimeout);
 
-    let testDirectory = "set_QA_performance";
-    let testFile = "cube_A_03200_z00001.fits";
     let timeEpoch: {time: number, thread: number, CPUusage: number}[] = [];
-    let startMeasures = delta();
+    let startMeasures: {tick: number, idle: number}[]; 
+    let endMeasures: {tick: number, idle: number}[];                   
     describe(`Change the number of thread: `, () => {
         [
             // [64], [48], [32], [24], 
@@ -72,12 +90,12 @@ describe("Image open performance: ", () => {
         ].map(
             function([threadNumber]: [number]) {
                 
-                test(`open "${testFile}" on backend with thread number = ${threadNumber}.`, 
+                test(`open "${arrayNext(imageFiles).next()}" on backend with thread number = ${threadNumber}.`, 
                 async done => {
                     let cartaBackend = child_process.exec(
-                        `"./carta_backend" folder=$HOME/CARTA/Images port=${port} threads=${threadNumber}`,
+                        `"./carta_backend" root=base base=${baseDirectory} port=${port} threads=${threadNumber}`,
                         {
-                            cwd: directory, 
+                            cwd: backendDirectory, 
                             timeout: 20000
                         }
                     );
@@ -106,6 +124,7 @@ describe("Image open performance: ", () => {
                                     OpenFileAck => {
                                         expect(OpenFileAck.success).toBe(true);
                                         timeElapsed = new Date().getTime() - timer;
+                                        endMeasures = delta();
                                         // console.log(`As thread number = ${threadNumber}. Elasped time = ${timeElapsed}ms`);
                                                                                 
                                         Connection.close();
@@ -114,7 +133,7 @@ describe("Image open performance: ", () => {
                                 Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
                                     {
                                         directory: testDirectory, 
-                                        file: testFile, 
+                                        file: arrayNext(imageFiles).current(), 
                                         hdu: "0", 
                                         fileId: 0, 
                                         renderMode: CARTA.RenderMode.RASTER,
@@ -131,15 +150,16 @@ describe("Image open performance: ", () => {
                             }
                         );
                         timer = new Date().getTime();
+                        startMeasures = delta();
                     };
 
                     Connection.onclose = () => {
-                        const endMeasures = delta();
                         const percentageCPU = endMeasures.map((end, i) => {
-                            return ((end.tick - startMeasures[i].tick) / (end.idle - startMeasures[i].idle) * 100);
+                            let tickDelta = end.tick - startMeasures[i].tick;
+                            return (tickDelta / (tickDelta + (end.idle - startMeasures[i].idle)) * 100);
                         });
-                        // console.log(percentageCPU.map(e => e + "%").join(` `), "\n");
-                        // console.log(percentageCPU.map(e => parseFloat(e)).reduce((a, b) => a + b) + "%", "\n");
+                        // console.log(percentageCPU.map(e => e.toPrecision(5) + "%").join(` `), "\n");
+                        // console.log(percentageCPU.reduce((a, b) => a + b).toPrecision(5) + "%", "\n");
 
                         timeEpoch.push({
                             time: timeElapsed, 
@@ -152,9 +172,8 @@ describe("Image open performance: ", () => {
 
                     cartaBackend.on("close", () => {
                         if (threadNumber === 2) {
-                            console.log(`Testing time: \n${timeEpoch.map(e => `\t${e.time} ms with CPU usage = \t${e.CPUusage.toFixed(5.2)}% as thread number = \t${e.thread}`).join(` \n`)}`);
-                        }
-                        
+                            console.log(`Testing time: \n${timeEpoch.map(e => `${e.time.toPrecision(5)} ms with CPU usage = ${e.CPUusage.toPrecision(5)}% as thread number = ${e.thread}`).join(` \n`)}`);
+                        }                      
 
                         done();
                     });
@@ -172,21 +191,10 @@ function delta() {
     return cpus.map(cpu => {
         const times = cpu.times;
         return {
-        tick: Object.keys(times).filter(time => time !== 'idle').reduce((tick, time) => { tick+=times[time]; return tick }, 0),
+        tick: Object.keys(times)
+                    .filter(time => time === "user")
+                    .reduce((tick, time) => { tick += times[time]; return tick; }, 0),
         idle: times.idle,
         };
     });
 }
-
-// let startMeasures = delta();
-// setInterval(() => {
-//   const endMeasures = delta();
-//   const percentageCPU = endMeasures.map((end, i) => {
-//     return ((end.tick - startMeasures[i].tick) / (end.idle - startMeasures[i].idle) * 100).toFixed(3) + "%";
-//   });
-
-//   console.log(percentageCPU.join(` `), "\n");
-
-//   // reset
-//   startMeasures = delta();
-// }, 2000);
