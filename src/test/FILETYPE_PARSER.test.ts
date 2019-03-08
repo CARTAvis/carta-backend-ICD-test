5673c0cc6a85b3d67adfa603753ef7dc56180764
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-03-08 16:26:42 +0800

    Add test for hdf5 file.

--- src/test/FILETYPE_PARSER.test.ts
+++ src/test/FILETYPE_PARSER.test.ts
@@ -122,11 +122,11 @@ describe("FILETYPE_PARSER test: Testing if all supported image types can be dete
 
         describe(`test the file is existent`, () => {
             [
-             ["SDC335.579-0.292.spw0.line.image", CARTA.FileType.CASA, 1864975311, [""]],
-             ["S255_IR_sci.spw25.cube.I.pbcor.fits", CARTA.FileType.FITS, 7048405440, ["0"]],
-             ["spire500_ext.fits", CARTA.FileType.FITS, 17591040, ["0", "1", "2", "3", "4", "5", "6", "7", ]],
-             ["G34mm1_lsb_all.uv.part1.line.natwt.sml", CARTA.FileType.MIRIAD, 34521240, [""]],
-	     //["orion_12co_hera.hdf5", CARTA.FileType.HDF5, 118888712, ["0"]],
+                ["SDC335.579-0.292.spw0.line.image", CARTA.FileType.CASA, 1864975311, [""]],
+                ["S255_IR_sci.spw25.cube.I.pbcor.fits", CARTA.FileType.FITS, 7048405440, ["0"]],
+                ["spire500_ext.fits", CARTA.FileType.FITS, 17591040, ["0", "1", "2", "3", "4", "5", "6", "7", ]],
+                ["G34mm1_lsb_all.uv.part1.line.natwt.sml", CARTA.FileType.MIRIAD, 34521240, [""]],
+	            ["orion_12co_hera.hdf5", CARTA.FileType.HDF5, 118888712, ["0"]],
             ].map(
                 function([file, type, size, hdu]:
                          [string, CARTA.FileType, number, string[]]) {
