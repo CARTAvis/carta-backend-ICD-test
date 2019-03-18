1792d7cdcaa40e6254400f373bcedd50f86aa69d
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-03-18 11:25:52 +0800

    Add performance test: Spatial profile on CPU monitor.

---
+++ src/performance/SpatialProfile_CPU1.test.ts
@@ -0,0 +1,251 @@
+import * as child_process from "child_process";
+import {CARTA} from "carta-protobuf";
+import * as Utility from "../UtilityFunction";
+import fileName from "./file.json";
+import config from "./config.json";
+import { cursorTo } from "readline";
+let pidusage = require("pidusage");
+
+let serverURL = config.serverURL;
+let port = config.port + 1000;
+let backendDirectory = config.path.backend;
+let baseDirectory = config.path.base;
+let testDirectory = config.path.performance;    
+let connectTimeout = config.timeout.connection;
+let openFileTimeout = config.timeout.openFile;
+let execWait = config.wait.exec;
+let psWait = config.wait.ps;
+let cursorWait = config.wait.cursor;
+let logMessage = config.log;
+let setCursorRepeat = config.repeat.cursor;
+let state = {index: -1};
+
+let testImageFiles = [
+    // fileName.imageFiles2fits,
+    // fileName.imageFiles4fits,
+    fileName.imageFiles8fits,
+    // fileName.imageFiles16fits,
+    // fileName.imageFiles32fits,
+    // fileName.imageFiles64fits,
+    // fileName.imageFiles128fits,
+];
+let testThreadNumber: number[] = [
+    64,
+    32,
+    24,
+    16,
+    12,
+    8,
+    4,
+    2,
+    1,
+];
+
+describe("Spatial profile performance: 1 user on 1 backend change thread number", () => {    
+    
+    test(`Preparing... dry run.`, 
+    done => {
+        
+        let cartaBackend = child_process.exec(
+            `./carta_backend root=base base=${baseDirectory} port=5678 threads=4`,
+            {
+                cwd: backendDirectory, 
+                timeout: 5000
+            }
+        );
+        cartaBackend.on("error", error => {
+            console.log(`error: ${error}`);
+        });
+        cartaBackend.stdout.on("data", data => {
+            if (logMessage) {
+                console.log(data);
+            }            
+        });      
+        
+        setTimeout(() => {
+            let Connection = new WebSocket(`${serverURL}:5678`);
+            Connection.binaryType = "arraybuffer";
+
+            Connection.onopen = () => {
+                Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
+                    RegisterViewerAck => {
+                        expect(RegisterViewerAck.success).toBe(true);
+                        
+                        Connection.close();
+                    }
+                );
+                Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+                    {
+                        sessionId: "", 
+                        apiKey: "1234"
+                    }
+                );
+            };
+            Connection.onclose = () => {
+                cartaBackend.kill();
+            };
+        }, 300);
+
+        cartaBackend.on("close", () => {
+            done();
+        });
+
+    }, connectTimeout);
+
+    testImageFiles.map(
+        (imageFiles: string[]) => {
+            let timeEpoch: {time: number, thread: number, CPUusage: number, RAM: number}[] = [];
+
+            describe(`Change the number of thread: `, () => {
+                testThreadNumber.map(
+                    (threadNumber: number) => {
+                        test(`set cursor randomly ${setCursorRepeat} times on "${Utility.arrayNext(imageFiles, state).next()}" on backend with thread number = ${threadNumber}.`, 
+                        done => {
+                            
+                            port += threadNumber;
+                            let cartaBackend = child_process.exec(
+                                `./carta_backend root=base base=${baseDirectory} port=${port} threads=${threadNumber}`,
+                                {
+                                    cwd: backendDirectory, 
+                                    timeout: openFileTimeout
+                                }
+                            );
+                            cartaBackend.on("error", error => {
+                                console.log(`error: \n ${error}`);
+                            });
+                            cartaBackend.stdout.on("data", data => {
+                                if (logMessage) {
+                                    console.log(data);
+                                }
+                            });
+                            
+                            let timer: number = 0;        
+                            let timeElapsed: number = 0;
+
+                            setTimeout(async () => {
+                                let Connection = await new WebSocket(`${serverURL}:${port}`);
+                                Connection.binaryType = "arraybuffer";
+                                
+                                Connection.onopen = () => {
+                                    Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
+                                        RegisterViewerAck => {
+                                            expect(RegisterViewerAck.success).toBe(true);
+                                            Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
+                                                (OpenFileAck: CARTA.OpenFileAck) => {
+                                                    if (!OpenFileAck.success) {
+                                                        console.error(OpenFileAck.message);
+                                                    }
+                                                    expect(OpenFileAck.success).toBe(true);
+                                                    Utility.getEvent(Connection, "RASTER_IMAGE_DATA", CARTA.RasterImageData, 
+                                                        RasterImageData => {
+                                                        expect(RasterImageData.imageData.length).toBeGreaterThan(0);
+                                                        let randPoint = {
+                                                            x: Math.floor(Math.random() * RasterImageData.imageBounds.xMax), 
+                                                            y: Math.floor(Math.random() * RasterImageData.imageBounds.yMax)};
+                                                        
+                                                        for ( let index = 0; index < setCursorRepeat; index++) {
+                                                            Utility.getEvent(Connection, "SPATIAL_PROFILE_DATA", CARTA.SpatialProfileData, 
+                                                                SpatialProfileData => {
+                                                                    expect(SpatialProfileData.profiles.length).not.toEqual(0);                                                        
+                                                                    timeElapsed += new Date().getTime() - timer;
+
+                                                                    if (index === setCursorRepeat - 1) {
+                                                                        Connection.close(); 
+                                                                    }
+                                                                    
+                                                                }
+                                                            );
+                                                            Utility.sleep(cursorWait);
+                                                            Utility.setEvent(Connection, "SET_CURSOR", CARTA.SetCursor, 
+                                                                {
+                                                                    fileId: 0, 
+                                                                    point: randPoint,
+                                                                }
+                                                            );
+                                                            timer = new Date().getTime();
+                                                            }
+                                                        }
+                                                    );
+                                                    Utility.setEvent(Connection, "SET_IMAGE_VIEW", CARTA.SetImageView, 
+                                                        {
+                                                            fileId: 0, 
+                                                            imageBounds: {
+                                                                xMin: 0, xMax: OpenFileAck.fileInfoExtended.width, 
+                                                                yMin: 0, yMax: OpenFileAck.fileInfoExtended.height,
+                                                            }, 
+                                                            mip: 64, 
+                                                            compressionType: CARTA.CompressionType.ZFP, 
+                                                            compressionQuality: 11, 
+                                                            numSubsets: 4,
+                                                        }
+                                                    );
+                                                    Utility.setEvent(Connection, "SET_SPATIAL_REQUIREMENTS", CARTA.SetSpatialRequirements, 
+                                                        {
+                                                            fileId: 0, 
+                                                            regionId: 0, 
+                                                            spatialProfiles: ["x", "y"],
+                                                        }
+                                                    );                                                    
+                                                }
+                                            );
+                                            Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
+                                                {
+                                                    directory: testDirectory, 
+                                                    file: Utility.arrayNext(imageFiles, state).current(), 
+                                                    hdu: "0", 
+                                                    fileId: 0, 
+                                                    renderMode: CARTA.RenderMode.RASTER,
+                                                }
+                                            );          
+                                        }
+                                    );
+
+                                    Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+                                        {
+                                            sessionId: "", 
+                                            apiKey: "1234"
+                                        }
+                                    );
+                                };
+                                
+                                Connection.onclose =  () => {
+                                    setTimeout( async () => {
+                                        let usage: {
+                                            cpu: number,
+                                            memory: number,
+                                            ppid: number,
+                                            pid: number,
+                                            ctime: number,
+                                            elapsed: number,
+                                            timestamp: number,
+                                        } = await pidusage(cartaBackend.pid);
+
+                                        timeEpoch.push({
+                                            time: timeElapsed, 
+                                            thread: threadNumber, 
+                                            CPUusage: usage.cpu,
+                                            RAM: usage.memory
+                                        });
+
+                                        cartaBackend.kill();
+                                    }, psWait);
+                                };
+                                
+                            }, execWait);
+
+                            cartaBackend.on("close", () => {
+                                if (threadNumber === testThreadNumber[testThreadNumber.length - 1]) {
+                                    console.log(`Backend testing outcome:\n${timeEpoch
+                                        .map(e => `${e.time.toPrecision(5)}ms with CPU usage = ${e.CPUusage.toPrecision(4)}% & RAM = ${e.RAM} bytes as thread# = ${e.thread}`).join(` \n`)}`);
+                                } 
+                                
+                                done();
+                            });
+                            
+                        }, openFileTimeout);
+                    }
+                );
+            });
+        }
+    );
+});    
