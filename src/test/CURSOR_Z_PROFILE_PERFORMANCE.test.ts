ec8952b04f9e571f347d095ba0d33461be01c8e9
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-03-22 17:30:27 +0800

    Refactor by async await.

--- src/test/CURSOR_Z_PROFILE_PERFORMANCE.test.ts
+++ src/test/CURSOR_Z_PROFILE_PERFORMANCE.test.ts
@@ -1,59 +1,57 @@
-/// Manual
+import {CARTA} from "carta-protobuf";
+import * as Utility from "./testUtilityFunction";
 import config from "./config.json";
 let testServerUrl = config.serverURL;
 let testSubdirectoryName = config.path.QA;
 let expectBasePath = config.path.base;
 let connectionTimeout = config.timeout.connection;
-let readFileTimeout = 20000;
-let testTimes = 10;
-
-/// ICD defined
-import {CARTA} from "carta-protobuf";
-import * as Utility from "./testUtilityFunction";
+let testTimes = config.repeat.cursor;
+let readLargeImageTimeout = config.timeout.readLargeImage;
 
 let baseDirectory: string;
 let count: number[][];
-let readPeriod = 200;
 
-describe("CURSOR_Z_PROFILE_PERFORMANCE tests", () => {   
+describe("CURSOR_Z_PROFILE_PERFORMANCE test: Testing the performance of cursor z profile generation", () => {   
     let Connection: WebSocket;
 
     beforeEach( done => {
-        // Establish a websocket connection in the binary form: arraybuffer 
         Connection = new WebSocket(testServerUrl);
+        expect(Connection.readyState).toBe(WebSocket.CONNECTING);
         Connection.binaryType = "arraybuffer";
-        // While open a Websocket
-        Connection.onopen = () => {
-            // Checkout if Websocket server is ready
-            if (Connection.readyState === WebSocket.OPEN) {
-                Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
+        Connection.onopen = OnOpen;
+
+        async function OnOpen (this: WebSocket, ev: Event) {
+            expect(this.readyState).toBe(WebSocket.OPEN);
+            await Utility.setEvent(this, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+                {
+                    sessionId: "", 
+                    apiKey: "1234"
+                }
+            );
+            await new Promise( resolve => { 
+                Utility.getEvent(this, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
                     RegisterViewerAck => {
                         expect(RegisterViewerAck.success).toBe(true);
-                        Utility.getEvent(Connection, "FILE_LIST_RESPONSE", CARTA.FileListResponse, 
-                            FileListResponseBase => {
-                                expect(FileListResponseBase.success).toBe(true);
-                                baseDirectory = FileListResponseBase.directory;
-                                done();
-                            }
-                        );
-                        Utility.setEvent(Connection, "FILE_LIST_REQUEST", CARTA.FileListRequest, 
-                            {
-                                directory: expectBasePath
-                            }
-                        );
+                        resolve();           
                     }
                 );
-                Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
-                    {
-                        sessionId: "", 
-                        apiKey: "1234"
+            });
+            await Utility.setEvent(this, "FILE_LIST_REQUEST", CARTA.FileListRequest, 
+                {
+                    directory: expectBasePath
+                }
+            );
+            await new Promise( resolve => {
+                Utility.getEvent(this, "FILE_LIST_RESPONSE", CARTA.FileListResponse, 
+                        FileListResponseBase => {
+                        expect(FileListResponseBase.success).toBe(true);
+                        baseDirectory = FileListResponseBase.directory;
+                        resolve();
                     }
-                );
-            } else {
-                console.log(`Can not open a connection. @${new Date()}`);
-                done();
-            }
-        };
+                );                
+            });
+            done();
+        }
     }, connectionTimeout);
     
     let mean: number[];
@@ -81,95 +79,100 @@ describe("CURSOR_Z_PROFILE_PERFORMANCE tests", () => {
                     }                    
                     
                     let timer: number;
-                    test(`assert a random cursor at round ${idx + 1} on the file "${testFileName}".`, 
-                    done => {
-                        Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
-                            OpenFileAck => {
-                                expect(OpenFileAck.success).toBe(true);
-                                Utility.getEvent(Connection, "RASTER_IMAGE_DATA", CARTA.RasterImageData, 
-                                    RasterImageData => {
-                                        expect(RasterImageData.imageData.length).toBeGreaterThan(0);
-                                        let randPoint = {
-                                            x: Math.floor(Math.random() * RasterImageData.imageBounds.xMax), 
-                                            y: Math.floor(Math.random() * RasterImageData.imageBounds.yMax)};
-                                        
-                                        Utility.getEvent(Connection, "SPECTRAL_PROFILE_DATA", CARTA.SpectralProfileData, 
-                                            SpectralProfileData => {
-                                                expect(SpectralProfileData.profiles.length).not.toEqual(0);                                                        
-                                                if (SpectralProfileData.profiles.length > 0) {
-                                                    count[fileIndex][idx] = new Date().getTime() - timer;                                                        
-                                                }
-
-                                                if (idx + 1 === testTimes) {
-                                                    let naturalCount = count[fileIndex].filter(e => e !== 0);
-                                                    mean[fileIndex] = naturalCount.reduce((a, b) => a + b, 0) / testTimes;
-                                                    squareDiffs[fileIndex] = naturalCount.map(function(value: number) {
-                                                            let diff = value - mean[fileIndex];
-                                                            return diff * diff;
-                                                        });
-                                                    SD[fileIndex] = Math.sqrt(squareDiffs[fileIndex].reduce((a, b) => a + b, 0) / squareDiffs[fileIndex].length);
-                                                    console.log(`for "${testFileName}": returning time = ${naturalCount} ms.  mean = ${mean[fileIndex]} ms.  deviation = ${SD[fileIndex]} ms. @${new Date()}`); 
-
-                                                } 
-                                                done();
-                                            }
-                                        );
-                                        Utility.sleep(readPeriod);
-                                        Utility.setEvent(Connection, "SET_CURSOR", CARTA.SetCursor, 
-                                            {
-                                                fileId: 0, 
-                                                point: randPoint,
-                                            }
-                                        );
-                                        timer = new Date().getTime();
-                                    }
-                                );
-                                Utility.setEvent(Connection, "SET_IMAGE_VIEW", CARTA.SetImageView, 
-                                    {
-                                        fileId: 0, 
-                                        imageBounds: {
-                                            xMin: 0, xMax: OpenFileAck.fileInfoExtended.width, 
-                                            yMin: 0, yMax: OpenFileAck.fileInfoExtended.height
-                                        }, 
-                                        mip, 
-                                        compressionType, 
-                                        compressionQuality, 
-                                        numSubsets,
-                                    }
-                                );
-                                Utility.setEvent(Connection, "SET_SPECTRAL_REQUIREMENTS", CARTA.SetSpectralRequirements, 
-                                    {
-                                        fileId: 0, 
-                                        regionId: 0, 
-                                        spectralProfiles: [
-                                            {
-                                                coordinate: "z", 
-                                                statsTypes: [CARTA.StatsType.None]
-                                            }
-                                        ],
-                                    }
-                                );
-                            }
-                        );
-                        Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
+                    test(`set a random cursor at round ${idx + 1} on the file "${testFileName}".`, 
+                    async () => {
+                        await Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
                             {
                                 directory: baseDirectory + "/" + testSubdirectoryName, 
                                 file: testFileName, 
                                 hdu: "0", 
                                 fileId: 0, 
-                                renderMode: CARTA.RenderMode.RASTER
+                                renderMode: CARTA.RenderMode.RASTER,
                             }
-                        ); 
-                    }, readFileTimeout); // test
+                        );
+                        let OpenFileAckTemp: CARTA.OpenFileAck; 
+                        await new Promise( resolve => {
+                            Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
+                                OpenFileAck => {
+                                    expect(OpenFileAck.success).toBe(true);
+                                    OpenFileAckTemp = OpenFileAck;
+                                    resolve();
+                                }
+                            );                
+                        });
+                        await Utility.setEvent(Connection, "SET_IMAGE_VIEW", CARTA.SetImageView, 
+                            {
+                                fileId: 0, 
+                                imageBounds: {
+                                    xMin: 0, xMax: OpenFileAckTemp.fileInfoExtended.width, 
+                                    yMin: 0, yMax: OpenFileAckTemp.fileInfoExtended.height,
+                                }, 
+                                mip, 
+                                compressionType, 
+                                compressionQuality, 
+                                numSubsets,
+                            }
+                        );
+                        await Utility.setEvent(Connection, "SET_SPECTRAL_REQUIREMENTS", CARTA.SetSpectralRequirements, 
+                            {
+                                fileId: 0, 
+                                regionId: 0, 
+                                spectralProfiles: [
+                                    {
+                                        coordinate: "z", 
+                                        statsTypes: [CARTA.StatsType.None]
+                                    }
+                                ],
+                            }
+                        );
+                        let RasterImageDataTemp: CARTA.RasterImageData;
+                        await new Promise( resolve => {
+                            Utility.getEvent(Connection, "RASTER_IMAGE_DATA", CARTA.RasterImageData, 
+                                RasterImageData => {
+                                    expect(RasterImageData.imageData.length).toBeGreaterThan(0);
+                                    RasterImageDataTemp = RasterImageData;
+                                    resolve();
+                                }
+                            );                
+                        });
+                        timer = await new Date().getTime();
+                        await Utility.setEvent(Connection, "SET_CURSOR", CARTA.SetCursor, 
+                            {
+                                fileId: 0, 
+                                point: {
+                                    x: Math.floor(Math.random() * RasterImageDataTemp.imageBounds.xMax), 
+                                    y: Math.floor(Math.random() * RasterImageDataTemp.imageBounds.yMax)
+                                },
+                            }
+                        );
+                        await new Promise( resolve => {                        
+                            Utility.getEvent(Connection, "SPECTRAL_PROFILE_DATA", CARTA.SpectralProfileData, 
+                                    SpectralProfileData => {
+                                    expect(SpectralProfileData.profiles.length).not.toEqual(0); 
+                                    resolve();
+                                }
+                            );
+                        });
+                        count[fileIndex][idx] = await new Date().getTime() - timer;
+                        if (idx + 1 === testTimes) {
+                            let naturalCount = count[fileIndex].filter(e => e !== 0);
+                            mean[fileIndex] = naturalCount.reduce((a, b) => a + b, 0) / testTimes;
+                            squareDiffs[fileIndex] = naturalCount.map(function(value: number) {
+                                    let diff = value - mean[fileIndex];
+                                    return diff * diff;
+                                });
+                            SD[fileIndex] = Math.sqrt(squareDiffs[fileIndex].reduce((a, b) => a + b, 0) / squareDiffs[fileIndex].length);
+                            console.log(`for "${testFileName}": returning time = ${naturalCount} ms.  mean = ${mean[fileIndex]} ms.  deviation = ${SD[fileIndex]} ms. @${new Date()}`); 
+                        }
+                    }, readLargeImageTimeout); // test
                     
                 }
             );
         }); // describe
     }
 
-    afterEach( done => {
+    afterEach( () => {
         Connection.close();
-        done();
-    }, connectionTimeout);
+    });
 
 });
