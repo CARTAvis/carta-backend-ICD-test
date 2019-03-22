7efe0378a56412f2d73e678e908443345fa4a41e
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-03-22 11:19:01 +0800

    Refactor by async await.

--- src/test/ACCESS_CARTA_UNKNOWN_APIKEY.test.ts
+++ src/test/ACCESS_CARTA_UNKNOWN_APIKEY.test.ts
@@ -1,97 +1,92 @@
-/// Manual
+import {CARTA} from "carta-protobuf";
+import * as Utility from "./testUtilityFunction";
 import config from "./config.json";
 let testServerUrl = config.serverURL;
 let connectTimeout = config.timeout.connection;
 
-/// ICD defined
-import {CARTA} from "carta-protobuf";
-import * as Utility from "./testUtilityFunction";
-
-let testEventName = "REGISTER_VIEWER";
-let testReturnName = "REGISTER_VIEWER_ACK";
-
 describe("ACCESS_CARTA_UNKNOWN_APIKEY tests", () => {
     
-    describe(`send EventName: "${testEventName}" to CARTA "${testServerUrl}" with session_id "" & api_key "5678".`, 
+    describe(`send EventName: "REGISTER_VIEWER" to CARTA "${testServerUrl}" with session_id "" & api_key "5678".`, 
     () => {
         let Connection: WebSocket;
     
         beforeEach( done => {
-            // Construct a Websocket
             Connection = new WebSocket(testServerUrl);
+            expect(Connection.readyState).toBe(WebSocket.CONNECTING);
             Connection.binaryType = "arraybuffer";
     
-            // While open a Websocket
-            Connection.onopen = () => {                
-                // Checkout if Websocket server is ready
-                if (Connection.readyState === WebSocket.OPEN) {
-                    // 
-                } else {
-                    console.log(`"${testServerUrl}" can not open a connection. @${Date.now()}`);
-                }
+            Connection.onopen = OnOpen;
+    
+            function OnOpen (this: WebSocket, ev: Event) {
+                expect(this.readyState).toBe(WebSocket.OPEN);
                 done();
-            };
+            }
         }, connectTimeout);
     
-        test(`assert the received EventName is "${testReturnName}" within ${connectTimeout} ms.`, 
-        done => {
-            // While receive a message from Websocket server
-            Connection.onmessage = (event: MessageEvent) => {
-                expect(event.data.byteLength).toBeGreaterThan(40);
-                
-                const eventName = Utility.getEventName(new Uint8Array(event.data, 0, 32));
-                expect(eventName).toBe(testReturnName);
-
-                done();
-            };
-            Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+        test(`assert the received EventName is "REGISTER_VIEWER_ACK" within ${connectTimeout} ms.`, 
+        async () => {
+            await Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
                 {
                     sessionId: "", 
                     apiKey: "5678"
                 }
             );
+            await new Promise( resolve => {
+                Connection.onmessage = (event: MessageEvent) => {
+                    expect(event.data.byteLength).toBeGreaterThan(40);
+                    
+                    const eventName = Utility.getEventName(new Uint8Array(event.data, 0, 32));
+                    expect(eventName).toBe("REGISTER_VIEWER_ACK");
+    
+                    resolve();
+                };
+            });
         }, connectTimeout);
                
-        test(`assert the "${testReturnName}.success" is true.`,
-        done => {
-            Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
-                RegisterViewerAck => {
-                    expect(RegisterViewerAck.success).toBe(true);
-                    done();
-                }
-            );
-            Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+        test(`assert the "REGISTER_VIEWER_ACK.success" is true.`,
+        async () => {
+            await Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
                 {
                     sessionId: "", 
                     apiKey: "5678"
                 }
             );
+            await new Promise( resolve => {
+                Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
+                    (RegisterViewerAck: CARTA.RegisterViewerAck) => {
+                        expect(RegisterViewerAck.success).toBe(true);
+                        resolve();
+                    }
+                );
+            });
         }, connectTimeout);
     
-        test(`assert the "${testReturnName}.message" is empty.`,
-        done => {
-            Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
-                RegisterViewerAck => {
-                    expect(RegisterViewerAck.message).toBeDefined();
-                    expect(RegisterViewerAck.message).toEqual("");
-                    if ( RegisterViewerAck.message !== "" ) {
-                        console.log(`"REGISTER_VIEWER_ACK.message" returns: "${RegisterViewerAck.message}" @${new Date()}`);
-                    }
-                    done();
-                }
-            );
-            Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+        test(`assert the "REGISTER_VIEWER_ACK.message" is empty.`,
+        async () => {
+            await Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
                 {
                     sessionId: "", 
                     apiKey: "5678"
                 }
             );
+            await new Promise( resolve => {
+                Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
+                    (RegisterViewerAck: CARTA.RegisterViewerAck) => {
+                        expect(RegisterViewerAck.message).toBeDefined();
+                        expect(RegisterViewerAck.message).toEqual("");
+                        if ( RegisterViewerAck.message !== "" ) {
+                            console.log(`"REGISTER_VIEWER_ACK.message" returns: "${RegisterViewerAck.message}" @${new Date()}`);
+                        }
+                        resolve();
+                    }
+                );
+            });
         }, connectTimeout);
 
-        afterEach( done => {
-            Connection.close();
-            done();
-        }, connectTimeout);    
+        afterEach( async () => {
+            await Connection.close();
+            await expect(Connection.readyState).toBe(WebSocket.CLOSING);
+        }, connectTimeout);   
     
     });
 });
