3fda2a33b74b0d5fcb3855cde313fe228783cad1
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-03-25 15:51:56 +0800

    Refactor by async await.

--- src/test/OPEN_IMAGE_MULTIFRAME.test.ts
+++ src/test/OPEN_IMAGE_MULTIFRAME.test.ts
@@ -1,4 +1,5 @@
-/// Manual
+import {CARTA} from "carta-protobuf";
+import * as Utility from "./testUtilityFunction";
 import config from "./config.json";
 let testServerUrl = config.serverURL;
 let testSubdirectoryName = config.path.QA;
@@ -7,54 +8,52 @@ let connectionTimeout = config.timeout.connection;
 let openFileTimeout = config.timeout.openFile;
 let readFileTimeout = config.timeout.readFile;
 
-/// ICD defined
-import {CARTA} from "carta-protobuf";
-import * as Utility from "./testUtilityFunction";
-
 let baseDirectory: string;
 
-describe("OPEN_IMAGE_MULTIFRAME tests", () => {   
+describe("OPEN_IMAGE_MULTIFRAME test: Testing the case of opening multiple images one by one without closing former ones", () => {   
     let Connection: WebSocket;
 
     beforeAll( done => {
-        // Establish a websocket connection in the binary form: arraybuffer 
         Connection = new WebSocket(testServerUrl);
+        expect(Connection.readyState).toBe(WebSocket.CONNECTING);
         Connection.binaryType = "arraybuffer";
-        // While open a Websocket
-        Connection.onopen = () => {
-            // Checkout if Websocket server is ready
-            if (Connection.readyState === WebSocket.OPEN) {                
-                Utility.getEvent(Connection, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
+        Connection.onopen = OnOpen;
+
+        async function OnOpen (this: WebSocket, ev: Event) {
+            expect(this.readyState).toBe(WebSocket.OPEN);
+            await Utility.setEvent(this, "REGISTER_VIEWER", CARTA.RegisterViewer, 
+                {
+                    sessionId: "", 
+                    apiKey: "1234"
+                }
+            );
+            await new Promise( resolve => { 
+                Utility.getEvent(this, "REGISTER_VIEWER_ACK", CARTA.RegisterViewerAck, 
                     RegisterViewerAck => {
                         expect(RegisterViewerAck.success).toBe(true);
-                        Utility.getEvent(Connection, "FILE_LIST_RESPONSE", CARTA.FileListResponse, 
-                            FileListResponseBase => {
-                                expect(FileListResponseBase.success).toBe(true);
-                                baseDirectory = FileListResponseBase.directory;
-                                done();
-                            }
-                        );
-                        Utility.setEvent(Connection, "FILE_LIST_REQUEST", CARTA.FileListRequest, 
-                            {
-                                directory: expectBasePath
-                            }
-                        );
+                        resolve();           
                     }
                 );
-                Utility.setEvent(Connection, "REGISTER_VIEWER", CARTA.RegisterViewer, 
-                    {
-                        sessionId: "", 
-                        apiKey: "1234"
+            });
+            await Utility.setEvent(this, "FILE_LIST_REQUEST", CARTA.FileListRequest, 
+                {
+                    directory: expectBasePath
+                }
+            );
+            await new Promise( resolve => {
+                Utility.getEvent(this, "FILE_LIST_RESPONSE", CARTA.FileListResponse, 
+                        FileListResponseBase => {
+                        expect(FileListResponseBase.success).toBe(true);
+                        baseDirectory = FileListResponseBase.directory;
+                        resolve();
                     }
-                );
-            } else {
-                console.log(`Can not open a connection. @${new Date()}`);
-                done();
-            }            
-        };
+                );                
+            });
+            done();   
+        }
     }, connectionTimeout);
 
-    describe(`read the files`, () => {
+    describe(`prepare to read files`, () => {
         [
             ["HH211_IQU_zoom_4ch.image.pbcor",             0,       "",        {xMin: 0, xMax:   251, yMin: 0, yMax:   251},  1],
             ["S255_IR_sci.spw25.cube.I.pbcor.fits",        1,      "0",        {xMin: 0, xMax:  1920, yMin: 0, yMax:  1920},  4],
@@ -63,19 +62,12 @@ describe("OPEN_IMAGE_MULTIFRAME tests", () => {
             function ([testFileName,    fileId,     hdu,    imageBounds,                                              mip]: 
                       [string,          number,     string, {xMin: number, xMax: number, yMin: number, yMax: number}, number]) {
                  
-                 describe(`read the file "${testFileName}"`, 
+                 describe(`read the file "${testFileName}" to`, 
                  () => {
 
-                    test(`assert the file "${testFileName}" to open.`, 
-                    done => {                    
-                        Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
-                            OpenFileAck => {
-                                expect(OpenFileAck.success).toBe(true);
-                                expect(OpenFileAck.fileId).toEqual(fileId);
-                                done();
-                            }
-                        );
-                        Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
+                    test(`assert to open the file "${testFileName}".`, 
+                    async () => {
+                        await Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
                             {
                                 directory: baseDirectory + "/" + testSubdirectoryName, 
                                 file: testFileName, 
@@ -84,17 +76,21 @@ describe("OPEN_IMAGE_MULTIFRAME tests", () => {
                                 renderMode: CARTA.RenderMode.RASTER,
                             }
                         );
+                        await new Promise( resolve => {
+                            Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
+                                OpenFileAck => {
+                                    expect(OpenFileAck.success).toBe(true);
+                                    expect(OpenFileAck.fileId).toEqual(fileId);
+                                    resolve();
+                                }
+                            );
+                            
+                        });
                     }, openFileTimeout);
                     
-                    test(`assert the file id of "${testFileName}" to be ${fileId}.`, 
-                    done => {
-                        Utility.getEvent(Connection, "RASTER_IMAGE_DATA", CARTA.RasterImageData, 
-                            RasterImageData => {
-                                expect(RasterImageData.fileId).toEqual(fileId);
-                                done();
-                            }
-                        );
-                        Utility.setEvent(Connection, "SET_IMAGE_VIEW", CARTA.SetImageView, 
+                    test(`assert the file ID of "${testFileName}" to be ${fileId}.`, 
+                    async () => {
+                        await Utility.setEvent(Connection, "SET_IMAGE_VIEW", CARTA.SetImageView, 
                             {
                                 fileId, 
                                 imageBounds: {
@@ -104,19 +100,25 @@ describe("OPEN_IMAGE_MULTIFRAME tests", () => {
                                 mip, 
                                 compressionType: CARTA.CompressionType.NONE,
                                 compressionQuality: 0, 
-                                numSubsets: 0, 
+                                numSubsets: 0,
                             }
                         );
-                    }, readFileTimeout); // test
+                        await new Promise( resolve => {
+                            Utility.getEvent(Connection, "RASTER_IMAGE_DATA", CARTA.RasterImageData, 
+                                RasterImageData => {
+                                    expect(RasterImageData.fileId).toEqual(fileId);
+                                    resolve();
+                                }
+                            );                
+                        });
+                    }, readFileTimeout);
 
-                });      
-
+                });  
             }
         );
     }); // describe
 
-    afterAll( done => {
+    afterAll( () => {
         Connection.close();
-        done();
     });
 });
