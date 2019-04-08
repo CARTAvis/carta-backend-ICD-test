6b1d4699477f5b1ce4a32df25eee34e1958d2abe
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-04-08 10:41:02 +0800

    Modify the testing task to run many times and get the average of result.

--- src/performance/ImageOpen_CPU1.test.ts
+++ src/performance/ImageOpen_CPU1.test.ts
@@ -9,16 +9,16 @@ let serverURL = config.serverURL;
 let port = config.port;
 let backendDirectory = config.path.backend;
 let baseDirectory = config.path.base;
-let testDirectory = config.path.performance;    
-let connectTimeout = config.timeout.connection;
+let testDirectory = config.path.performance;
 let openFileTimeout = config.timeout.openFile;
 let reconnectWait = config.wait.reconnect;
+let repeatEvent = config.repeat.event;
 let logMessage = config.log;
 
 let testImageFiles = [
-    fileName.imageFiles2fits,
+    // fileName.imageFiles2fits,
     // fileName.imageFiles4fits,
-    // fileName.imageFiles8fits,
+    fileName.imageFiles8fits,
     // fileName.imageFiles16fits,
     // fileName.imageFiles32fits,
     // fileName.imageFiles64fits,
@@ -91,28 +91,31 @@ describe("Image open performance: 1 user on 1 backend change thread number", () 
                                     }
                                 );
                             });
-                            await Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
-                                {
-                                    directory: testDirectory, 
-                                    file: imageFileNext, 
-                                    hdu: "0", 
-                                    fileId: 0, 
-                                    renderMode: CARTA.RenderMode.RASTER,
-                                }
-                            );
-                            let timer = await performance.now(); 
-                            await new Promise( resolve => {
-                                Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
-                                    (OpenFileAck: CARTA.OpenFileAck) => {
-                                        if (!OpenFileAck.success) {
-                                            console.error(OpenFileAck.fileInfo.name + " : " + OpenFileAck.message);
-                                        }
-                                        expect(OpenFileAck.success).toBe(true);                                            
-                                        resolve();
+                            let timeElapsed: number[] = [];
+                            for (let idx = 0; idx < repeatEvent; idx++) {
+                                await Utility.setEvent(Connection, "OPEN_FILE", CARTA.OpenFile, 
+                                    {
+                                        directory: testDirectory, 
+                                        file: imageFilesGenerator.next().value,
+                                        hdu: "0", 
+                                        fileId: 0, 
+                                        renderMode: CARTA.RenderMode.RASTER,
                                     }
                                 );
-                            });
-                            let timeElapsed = await performance.now() - timer;
+                                let timer = await performance.now(); 
+                                await new Promise( resolve => {
+                                    Utility.getEvent(Connection, "OPEN_FILE_ACK", CARTA.OpenFileAck, 
+                                        (OpenFileAck: CARTA.OpenFileAck) => {
+                                            if (!OpenFileAck.success) {
+                                                console.error(OpenFileAck.fileInfo.name + " : " + OpenFileAck.message);
+                                            }
+                                            expect(OpenFileAck.success).toBe(true);                                            
+                                            resolve();
+                                        }
+                                    );
+                                });
+                                timeElapsed.push(await performance.now() - timer);
+                            }
 
                             await Connection.close();                            
 
@@ -127,11 +130,11 @@ describe("Image open performance: 1 user on 1 backend change thread number", () 
                             } = await pidusage(cartaBackend.pid);
                             
                             timeEpoch.push({
-                                time: timeElapsed, 
+                                time: timeElapsed.reduce((a, b) => a + b) / timeElapsed.length, 
                                 thread: threadNumber, 
-                                CPUusage: usage.ctime,
+                                CPUusage: usage.ctime / timeElapsed.length,
                                 RAM: usage.memory
-                            });               
+                            });
                             
                             await cartaBackend.kill();
 
@@ -139,7 +142,7 @@ describe("Image open performance: 1 user on 1 backend change thread number", () 
                                 cartaBackend.on("close", () => {
                                     if (threadNumber === testThreadNumber[testThreadNumber.length - 1]) {
                                         console.log(`Backend testing outcome:\n${timeEpoch
-                                            .map(e => `${e.time.toPrecision(5)}ms with CPU usage = ${e.CPUusage.toFixed(4)}ms & RAM = ${e.RAM}bytes as thread# = ${e.thread}`).join(` \n`)}`);
+                                            .map(e => `${e.time.toPrecision(5)}ms with CPU usage = ${e.CPUusage.toFixed(3)}ms & RAM = ${e.RAM}bytes as thread# = ${e.thread}`).join(` \n`)}`);
                                     }                                 
                                     resolve();
                                 });
