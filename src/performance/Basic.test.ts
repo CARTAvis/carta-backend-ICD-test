0f86adf14c508110daeaeea95a2a3e0337c962bf
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-04-15 10:10:34 +0800

    Add test for node package: usage-node

--- src/performance/Basic.test.ts
+++ src/performance/Basic.test.ts
@@ -3,6 +3,7 @@ import {CARTA} from "carta-protobuf";
 import * as Utility from "../UtilityFunction";
 import config from "./config.json";
 import * as SocketOperation from "./SocketOperation";
+let nodeusage = require("usage");
 
 let serverURL = config.serverURL;
 let port = config.port;
@@ -17,6 +18,8 @@ describe("Basic test: ", () => {
     test(`should connect a prepared backend.`, 
     async () => {
 
+        let tested = false;
+
         let cartaBackend = await SocketOperation.CartaBackend(
             baseDirectory, port, 5, backendDirectory, connectTimeout, logMessage);
         
@@ -24,12 +27,42 @@ describe("Basic test: ", () => {
 
         await SocketOperation.RegisterViewer(Connection, async () => {
             // console.log("done");
+            tested = true;
         });
+
+        expect(await tested).toBe(true);
 
         await Connection.close();
         
         await cartaBackend.kill();
         
     }, connectTimeout);
+
+});
+
+describe(`node-usage test`, () => {
+    test(`should read CPU usage`, 
+    () => {
+        nodeusage.lookup(
+            process.pid, 
+            (err, result) => {
+                return () => {
+                    expect(result.cpu).toBeGreaterThan(0);
+                };
+            }
+        );
+    });
     
-});    
+    test(`should read RAM usage`, 
+    () => {
+        nodeusage.lookup(
+            process.pid, 
+            (err, result) => {
+                return () => {
+                    expect(result.memory).toBeGreaterThan(0);
+                };
+            }
+        );
+    });
+
+});
