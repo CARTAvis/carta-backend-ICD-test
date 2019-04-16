7cfbdb41994acdea4aa4bce26ab1ecd268f73298
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-04-16 17:31:55 +0800

    Add performance test: ImageRender_Disk1

---
+++ src/performance/ImageRender_Disk1.test.ts
@@ -0,0 +1,106 @@
+import * as Utility from "../UtilityFunction";
+import fileName from "./file.json";
+import config from "./config.json";
+import * as SocketOperation from "./SocketOperation";
+let nodeusage = require("usage");
+let procfs = require("procfs-stats");
+
+let serverURL = config.serverURL;
+let port = config.port;
+let backendDirectory = config.path.backend;
+let baseDirectory = config.path.base;
+let testDirectory = config.path.performance;
+let openFileTimeout = config.timeout.openFile;
+let reconnectWait = config.wait.reconnect;
+let logMessage = config.log;
+
+let threadNumber = 16;
+let imageFilesGroup = [
+    fileName.imageFilesAfits,
+    // fileName.imageFilesBfits,
+    // fileName.imageFilesCfits,
+
+    // fileName.imageFilesAimage,
+    // fileName.imageFilesBimage,
+    // fileName.imageFilesCimage,
+
+    // fileName.imageFilesAhdf5,
+    // fileName.imageFilesBhdf5,
+    // fileName.imageFilesChdf5,
+];
+
+describe("Image render performance:  1 user on 1 backend change image size", () => {    
+     
+    let timeEpoch: {time: number, thread: number, CPUusage: number, RAM: number, fileName: string, Disk: number}[] = [];
+    imageFilesGroup.map(
+        (imageFiles: string[]) => {
+            let imageFilesGenerator = Utility.arrayGeneratorLoop(imageFiles);
+            describe(`Change the image size as thread = ${threadNumber}: `, () => {
+                imageFiles.map(
+                    (imageFile: string) => {                
+                        let imageFileNext = imageFilesGenerator.next().value;
+                        test(`should render "${imageFile}" on backend.`, 
+                        async done => {
+                            let cartaBackend = await SocketOperation.CartaBackend(
+                                baseDirectory, port, threadNumber, backendDirectory, openFileTimeout, logMessage);
+
+                            let Connection = await SocketOperation.SocketClient(serverURL, port, reconnectWait);
+                        
+                            await SocketOperation.RegisterViewer(Connection);
+
+                            let timeElapsed: number = 0;
+                            let OpenFileAckTemp = 
+                                await SocketOperation.OpenFile(
+                                    Connection, 
+                                    testDirectory, 
+                                    imageFileNext,
+                                );
+                            await SocketOperation.SetImageView(
+                                Connection, 
+                                OpenFileAckTemp, 
+                                async timer => {
+                                    timeElapsed = await performance.now() - timer;
+                                }
+                            );
+                            
+                            await Connection.close();  
+                                                      
+                            let diskR: number;
+                            await new Promise( resolve => {
+                                procfs(cartaBackend.pid).io( (err, io) => {
+                                    diskR = io.rchar;
+                                    resolve();
+                                });
+                            });
+                            await new Promise( resolve => {
+                                nodeusage.lookup(
+                                    cartaBackend.pid, 
+                                    (err, result) => {                                        
+                                        timeEpoch.push({
+                                            time: timeElapsed, 
+                                            thread: threadNumber, 
+                                            CPUusage: result.cpu,
+                                            RAM: result.memory,
+                                            fileName: imageFileNext,
+                                            Disk: diskR,
+                                        });
+                                        resolve();
+                                    }
+                                );
+                            });            
+                            
+                            await cartaBackend.kill();
+                            
+                            cartaBackend.on("close", () => done());
+
+                        }, openFileTimeout);
+                    }
+                );
+            });
+        }
+    );
+    
+    afterAll( () => {
+        SocketOperation.OutcomeWithDiskIO(timeEpoch);
+    });
+});    
