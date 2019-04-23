356e92a6a82d77d4ea394b9b6b8c4d46bdc7dfd0
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-04-23 14:55:25 +0800

    Add performance test: ImageOpen_Unit and test file generator element on testFIleConfig.json

---
+++ src/performance/ImageOpen_Unit.test.ts
@@ -0,0 +1,89 @@
+import * as Utility from "../UtilityFunction";
+import fileName from "./file.json";
+import config from "./config.json";
+import fileConfig from "./testFileConfig.json";
+import * as SocketOperation from "./SocketOperation";
+
+let serverURL = config.serverURL;
+let port = config.port;
+let backendDirectory = config.path.backend;
+let baseDirectory = config.path.base;
+let testDirectory = config.path.performance;
+let openFileTimeout = config.timeout.openFile;
+let reconnectWait = config.wait.reconnect;
+let repeatEvent = config.repeat.event;
+let logMessage = config.log;
+
+let testThreadNumber: number[] = fileConfig.thread;
+let testImageFiles =
+fileConfig.fileFormat.map(
+    format => {
+        // return fileConfig.channelSize.map(
+        //     channel => {
+                return fileConfig.imageSize.map(
+                    image => {
+                        return fileConfig.dataSource.map(
+                            name => {
+                                return `cube_${name}/cube_${name}_` + 
+                                `${image.toLocaleString("en-US", {minimumIntegerDigits: 6, useGrouping: false})}` +
+                                `_z00001.${format}`;
+                            }
+                        );
+                    }
+                );
+        //     }
+        // ).reduce((acc, val) => acc.concat(val));
+    }
+).reduce((acc, val) => acc.concat(val));
+
+describe("Image open performance: 1 user on 1 backend change thread number", () => {    
+    
+    let epoch: number[][] = [[]];
+    testImageFiles.map(
+        (imageFiles: string[]) => {
+            let imageFilesGenerator = Utility.arrayGeneratorLoop(imageFiles);
+            epoch.push([]);
+            describe(`Change the number of thread on file ${imageFiles[0].slice(14)}:`, () => {
+                testThreadNumber.map(
+                    (threadNumber: number) => {
+                        let imageFileNext = imageFilesGenerator.next().value;
+                        test(`open image "${imageFileNext}" on backend with thread number = ${threadNumber}.`, 
+                        async done => {                            
+                            let cartaBackend = await SocketOperation.CartaBackend(
+                                baseDirectory, port, threadNumber, backendDirectory, openFileTimeout, logMessage);
+                            
+                            let Connection = await SocketOperation.SocketClient(serverURL, port, reconnectWait);
+                            
+                            await SocketOperation.RegisterViewer(Connection);
+
+                            let timeElapsed: number[] = [];
+                            for (let idx = 0; idx < repeatEvent; idx++) {
+                                await SocketOperation.OpenFile(
+                                    Connection, 
+                                    testDirectory, 
+                                    imageFileNext,
+                                    async timer => {
+                                        timeElapsed.push(await performance.now() - timer);
+                                    } 
+                                );
+                            }
+                            epoch[testImageFiles.indexOf(imageFiles)]
+                                .push(timeElapsed.reduce((a, b) => a + b) / timeElapsed.length);
+
+                            await Connection.close();
+                            
+                            await cartaBackend.kill();
+
+                            cartaBackend.on("close", () => done());
+
+                        }, openFileTimeout);
+                    }
+                );
+            });
+        }
+    );
+
+    afterAll( () => {
+        console.log(epoch);
+    });
+});    
