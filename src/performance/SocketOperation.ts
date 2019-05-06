2c880a288a019af49ae469f61c9aafe13b0c2021
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-05-06 17:53:29 +0800

    Fix ratio to be percentage

--- src/performance/SocketOperation.ts
+++ src/performance/SocketOperation.ts
@@ -82,7 +82,7 @@ WriteReportTo(
             });
         });
     }
-    let cpuUsage = cpuCount.user ? cpuCount.user : (cpuCount.user / cpuCount.total);
+    let cpuUsage = cpuCount.user ? cpuCount.user : (100 * cpuCount.user / cpuCount.total);
     await new Promise( resolve => {
         nodeusage.lookup(
             pid, 
