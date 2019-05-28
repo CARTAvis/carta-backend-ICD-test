6f95e34873589aea9d88ab3f93b1d919391159e2
Hengtai <zarda@users.noreply.github.com>
2019-05-28 17:01:06 +0800

    Issue #3 session and api key (#27)
    * Fix config.serverURL
    
    * Refactor and let websocket testing to be standalone.
    
    * Add testing: ACCESS_CARTA_UNKNOWN_SESSION
    
    * Fix as adding close()
    
    * Fix by adding await
    
    * Add testing: ACCESS_CARTA_DEFAULT
    
    * Fix description
    
    * Modify to update
    
    * Refactor for lint unity

--- src/test/ACCESS_CARTA_UNKNOWN_SESSION.test.ts
+++ src/test/ACCESS_CARTA_UNKNOWN_SESSION.test.ts
@@ -4,13 +4,12 @@ import config from "./config.json";
 let testServerUrl = config.serverURL;
 let connectTimeout = config.timeout.connection;
 
-describe("ACCESS_CARTA_UNKNOWN_APIKEY tests: Testing connections to the backend with an unknown API key", () => {
-    let Connection: WebSocket;
-    describe(`send "REGISTER_VIEWER" to "${testServerUrl}" with session_id=0 & api_key="1234"`, () => {
+describe("ACCESS_CARTA_UNKNOWN_SESSION tests: Testing connections to the backend with an unknown session id", () => {
+    describe(`send "REGISTER_VIEWER" to "${testServerUrl}" with session_id=9999 & api_key=""`, () => {
         let RegisterViewerAckTemp: CARTA.RegisterViewerAck; 
         test(`should get "REGISTER_VIEWER_ACK" within ${connectTimeout} ms`, done => {        
             // Connect to "testServerUrl"
-            Connection = new WebSocket(testServerUrl);
+            let Connection = new WebSocket(testServerUrl);
             expect(Connection.readyState).toBe(WebSocket.CONNECTING);
 
             Connection.binaryType = "arraybuffer";
@@ -20,8 +19,8 @@ describe("ACCESS_CARTA_UNKNOWN_APIKEY tests: Testing connections to the backend 
                 expect(this.readyState).toBe(WebSocket.OPEN);
                 await Utility.setEvent(this, CARTA.RegisterViewer,
                     {
-                        sessionId: 0, 
-                        apiKey: "1234",
+                        sessionId: 9999, 
+                        apiKey: "",
                     }
                 );
                 await new Promise( resolve => {
@@ -37,23 +36,21 @@ describe("ACCESS_CARTA_UNKNOWN_APIKEY tests: Testing connections to the backend 
             }
         }, connectTimeout);
 
-        test("REGISTER_VIEWER_ACK.success = True", () => {
-            expect(RegisterViewerAckTemp.success).toBe(true);
-        });
-
-        test("REGISTER_VIEWER_ACK.session_id is not None", () => {
-            expect(RegisterViewerAckTemp.sessionId).toBeDefined();
-            console.log(`Registered session ID is ${RegisterViewerAckTemp.sessionId} @${new Date()}`);
+        test("REGISTER_VIEWER_ACK.success = False", () => {
+            expect(RegisterViewerAckTemp.success).toBe(false);
         });
 
         test(`REGISTER_VIEWER_ACK.session_type = "CARTA.SessionType.NEW"`, () => {
-            expect(RegisterViewerAckTemp.sessionType).toBe(CARTA.SessionType.NEW);
+            expect(RegisterViewerAckTemp.sessionType).toBe(CARTA.SessionType.RESUMED);
         });
 
-        test("REGISTER_VIEWER_ACK.message is empty", () => {
-            expect(RegisterViewerAckTemp.message).toBe("");
+        test("REGISTER_VIEWER_ACK.message is not empty", () => {
+            expect(RegisterViewerAckTemp.message).toBeDefined();
+            expect(RegisterViewerAckTemp.message).not.toEqual("");
+            if ( RegisterViewerAckTemp.message !== "" ) {
+                console.log(`"REGISTER_VIEWER_ACK.message" returns: "${RegisterViewerAckTemp.message}" @${new Date()}`);
+            }
         });
 
     });
-
 });
---
+++ src/test/ACCESS_WEBSOCKET.test.ts
@@ -0,0 +1,41 @@
+import {CARTA} from "carta-protobuf";
+import * as Utility from "./testUtilityFunction";
+import config from "./config.json";
+let testServerUrl = config.serverURL;
+let connectTimeout = config.timeout.connection;
+
+describe("ACCESS_WEBSOCKET tests: Testing connections to the websocket server", () => {
+    let testRemoteWebsocketSite = "wss://echo.websocket.org";
+    test(`should connect to "${testRemoteWebsocketSite}".`, 
+    done => {
+        // Construct a Websocket
+        let Connection = new WebSocket(testRemoteWebsocketSite);
+
+        // While open a Websocket
+        Connection.onopen = () => {
+            Connection.close();
+            done();     // Return to this test
+        };
+    }, connectTimeout);
+
+    test(`should connect to "${testServerUrl}".`, done => {
+
+        let Connection = new WebSocket(testServerUrl);
+        expect(Connection.readyState).toBe(WebSocket.CONNECTING);
+        
+        Connection.onopen = OnOpen;
+        Connection.onclose = OnClose;
+
+        function OnOpen (this: WebSocket, ev: Event) {
+            expect(this.readyState).toBe(WebSocket.OPEN);
+            
+            this.close();
+            expect(this.readyState).toBe(WebSocket.CLOSING);
+        }
+        function OnClose (this: WebSocket, ev: CloseEvent) {
+            expect(this.readyState).toBe(WebSocket.CLOSED);
+            done();
+        }
+
+    }, connectTimeout);
+});
