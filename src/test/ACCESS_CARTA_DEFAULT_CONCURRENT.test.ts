03b57665532328856f84a5ae993280ee5f5cc5c4
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-11-29 09:42:27 +0800

    Refine code

--- src/test/ACCESS_CARTA_DEFAULT_CONCURRENT.test.ts
+++ src/test/ACCESS_CARTA_DEFAULT_CONCURRENT.test.ts
@@ -22,190 +22,80 @@ let assertItem: AssertItem = {
 
 let client: Client[] = Array(testNumber);
 
-describe("Access Websocket concurrently test1", () => {
-
-    test(`establish ${testNumber} connections to "${testServerUrl}".`, done => {
-
-        let clientPromise: Promise<void>[] = Array(testNumber).fill(Promise);
-
-        for (let i = 0; i< client.length; i++) {
-            client[i] = new Client(testServerUrl);
-            expect(client[i].connection.readyState).toBe(WebSocket.CONNECTING);
-
-            clientPromise[i] = client[i].open(connectionTimeout).then( ()=>{
-                expect(client[i].connection.readyState).toBe(WebSocket.OPEN);
-            });
-        }
-
-        Promise.all(clientPromise).then( () => done() );
-
-    }, concurrentTimeout);
-
-    test(`close ${testNumber} connections from "${testServerUrl}".`, done => {
-
-        let clientPromise: Promise<void>[] = Array(testNumber).fill(Promise);
-
-        for (let i = 0; i< client.length; i++) {
-            clientPromise[i] = client[i].close().then( ()=>{
-                expect(client[i].connection.readyState).toBe(WebSocket.CLOSED);
-            });
-        }
-
-        Promise.all(clientPromise).then( () => done() );
-
-    }, concurrentTimeout);
-        
-});
-
-describe("Access Websocket concurrently test2", () => {
+describe("Access Websocket concurrently", () => {
 
     test(`establish ${testNumber} connections to "${testServerUrl}".`, async () => {
 
-        for (let i = 0; i< client.length; i++) {
+        for (let i = 0; i < client.length; i++) {
             client[i] = new Client(testServerUrl);
             expect(client[i].connection.readyState).toBe(WebSocket.CONNECTING);
 
-            await client[i].open(connectionTimeout).then( ()=>{
-                expect(client[i].connection.readyState).toBe(WebSocket.OPEN);
-            });
+            await client[i].open(connectionTimeout).then(() =>
+                expect(client[i].connection.readyState).toBe(WebSocket.OPEN)
+            );
         }
 
     }, concurrentTimeout);
 
     test(`close ${testNumber} connections from "${testServerUrl}".`, async () => {
 
-        for (let i = 0; i< client.length; i++) {
-            await client[i].close().then( ()=>{
-                expect(client[i].connection.readyState).toBe(WebSocket.CLOSED);
-            });
+        for (let i = 0; i < client.length; i++) {
+            await client[i].close().then(() =>
+                expect(client[i].connection.readyState).toBe(WebSocket.CLOSED)
+            );
         }
 
     }, concurrentTimeout);
-        
-});
-
-describe("ACCESS_CARTA_DEFAULT_CONCURRENT test1: Testing multiple concurrent connections to the backend.", () => {
-    
-    test(`establish ${testNumber} connections to "${testServerUrl}".`, done => {
-
-        let clientPromise: Promise<void>[] = Array(testNumber).fill(Promise);
-
-        for (let i = 0; i< client.length; i++) {
-            client[i] = new Client(testServerUrl);
-            expect(client[i].connection.readyState).toBe(WebSocket.CONNECTING);
-
-            clientPromise[i] = client[i].open(connectionTimeout).then( ()=>{
-                expect(client[i].connection.readyState).toBe(WebSocket.OPEN);
-            });
-        }
-
-        Promise.all(clientPromise).then( () => done() );
-
-    }, concurrentTimeout);
-
-    let registerViewerAck: CARTA.RegisterViewerAck[] = Array(testNumber).fill(CARTA.RegisterViewerAck);
-
-    test(`${testNumber} connections send EventName: "REGISTER_VIEWER" to CARTA "${testServerUrl}".`, done => {
-
-        let clientPromise: Promise<CARTA.RegisterViewerAck>[] = Array(testNumber).fill(Promise);
-
-        for(let index = 0; index < client.length; index++) {
-            client[index].send(CARTA.RegisterViewer, assertItem.register);
-            clientPromise[index] = client[index].receive(CARTA.RegisterViewerAck) as Promise<CARTA.RegisterViewerAck>;
-        }
-
-        Promise.all(clientPromise).then( value => {
-            registerViewerAck = value;
-            done() ;
-        });
-
-    }, concurrentTimeout);
-
-    test(`assert every REGISTER_VIEWER_ACK.success to be True.`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
-            expect(item.success).toBe(true);
-        });
-    });
-
-    test(`assert every REGISTER_VIEWER_ACK.session_id is not None.`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
-            expect(item.sessionId).toBeDefined();
-        });
-    });
-
-    test(`assert every REGISTER_VIEWER_ACK.session_id is unique.`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
-            expect(array.filter(f => f.sessionId === item.sessionId).length).toEqual(1);
-        });
-    });
-
-    test(`assert every REGISTER_VIEWER_ACK.session_type is "CARTA.SessionType.NEW".`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
-            expect(item.sessionType).toEqual(CARTA.SessionType.NEW);
-        });
-    });
-        
-    afterAll( done => {
-        
-        let clientPromise: Promise<void>[] = Array(testNumber).fill(Promise);
-
-        for (let i = 0; i< client.length; i++) {
-            clientPromise[i] = client[i].close();
-        }
-
-        Promise.all(clientPromise).then( () => done() );
-    });
 
 });
 
+describe("ACCESS_CARTA_DEFAULT_CONCURRENT: Testing multiple concurrent connections to the backend.", () => {
 
-describe("ACCESS_CARTA_DEFAULT_CONCURRENT test2: Testing multiple concurrent connections to the backend.", () => {
-    
     test(`establish ${testNumber} connections to "${testServerUrl}".`, async () => {
-        for (let i = 0; i< client.length; i++) {
+        for (let i = 0; i < client.length; i++) {
             client[i] = new Client(testServerUrl);
             expect(client[i].connection.readyState).toBe(WebSocket.CONNECTING);
-            await client[i].open(connectionTimeout).then(()=>{
-                expect(client[i].connection.readyState).toBe(WebSocket.OPEN);
-            });
+            await client[i].open(connectionTimeout).then(() =>
+                expect(client[i].connection.readyState).toBe(WebSocket.OPEN)
+            );
         }
     }, concurrentTimeout);
 
-    let registerViewerAck: CARTA.RegisterViewerAck[] = Array(testNumber).fill(CARTA.RegisterViewerAck);
+    let registerViewerAck: CARTA.RegisterViewerAck[] = [];
 
-    test(`${testNumber} connections send EventName: "REGISTER_VIEWER" to CARTA "${testServerUrl}" with no session_id & api_key "1234".`, async () => {
-        for(let index = 0; index < client.length; index++) {
-            await client[index].send(CARTA.RegisterViewer, assertItem.register);
-            registerViewerAck[index] = await client[index].receive(CARTA.RegisterViewerAck) as CARTA.RegisterViewerAck;
+    test(`${testNumber} connections send EventName: "REGISTER_VIEWER" to CARTA "${testServerUrl}".`, async () => {
+        for (let index = 0; index < client.length; index++) {
+            client[index].send(CARTA.RegisterViewer, assertItem.register);
+            registerViewerAck.push(await client[index].receive(CARTA.RegisterViewerAck));
         }
     }, concurrentTimeout);
 
-    test(`assert every REGISTER_VIEWER_ACK.success to be True.`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
+    test(`assert every REGISTER_VIEWER_ACK.success is True.`, () => {
+        registerViewerAck.forEach((item, index, array) => {
             expect(item.success).toBe(true);
         });
     });
 
     test(`assert every REGISTER_VIEWER_ACK.session_id is not None.`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
+        registerViewerAck.forEach((item, index, array) => {
             expect(item.sessionId).toBeDefined();
         });
     });
 
     test(`assert every REGISTER_VIEWER_ACK.session_id is unique.`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
+        registerViewerAck.forEach((item, index, array) => {
             expect(array.filter(f => f.sessionId === item.sessionId).length).toEqual(1);
         });
     });
 
     test(`assert every REGISTER_VIEWER_ACK.session_type is "CARTA.SessionType.NEW".`, () => {
-        registerViewerAck.forEach( (item, index, array) => {
+        registerViewerAck.forEach((item, index, array) => {
             expect(item.sessionType).toEqual(CARTA.SessionType.NEW);
         });
     });
-        
-    afterAll( async () => {
-        for (let i = 0; i< client.length; i++) {
+
+    afterAll(async () => {
+        for (let i = 0; i < client.length; i++) {
             await client[i].close();
         }
     });
