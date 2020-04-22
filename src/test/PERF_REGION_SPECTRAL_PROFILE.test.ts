0036384a523c856d1540638b70155d39d8de0c6e
mylin-asiaa-ubuntu <acdo2002@gmail.com>
2020-04-22 16:46:46 +0800

    Finish PERF_REGION_SPECTRAL_PROFILE.test.ts

--- src/test/PERF_REGION_SPECTRAL_PROFILE.test.ts
+++ src/test/PERF_REGION_SPECTRAL_PROFILE.test.ts
@@ -6,7 +6,7 @@ let testServerUrl: string = config.serverURL;
 let testSubdirectory: string = config.path.performance;
 let connectTimeout: number = config.timeout.connection;
 let openFileTimeout: number = config.timeout.openFile;
-let readFileTimeout: number = config.timeout.readFile;
+let readFileTimeout: number = 80000;//config.timeout.readFile;
 let readRegionTimeout: number = config.timeout.region;
 
 interface AssertItem {
@@ -33,125 +33,125 @@ let assertItem: AssertItem = {
             fileId: 0,
             renderMode: CARTA.RenderMode.RASTER,
         },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z02000.fits",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z04000.fits",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z01000.fits",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z02000.fits",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z04000.fits",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z01000.image",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z02000.image",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z04000.image",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z01000.image",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z02000.image",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z04000.image",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z01000.hdf5",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z02000.hdf5",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_01600_z04000.hdf5",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z01000.hdf5",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z02000.hdf5",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
-        // {
-        //     directory: testSubdirectory + "/cube_A",
-        //     file: "cube_A_03200_z04000.hdf5",
-        //     hdu: "",
-        //     fileId: 0,
-        //     renderMode: CARTA.RenderMode.RASTER,
-        // },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z02000.fits",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z04000.fits",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z01000.fits",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z02000.fits",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z04000.fits",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z01000.image",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z02000.image",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z04000.image",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z01000.image",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z02000.image",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z04000.image",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z01000.hdf5",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z02000.hdf5",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_01600_z04000.hdf5",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z01000.hdf5",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z02000.hdf5",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+        {
+            directory: testSubdirectory + "/cube_A",
+            file: "cube_A_03200_z04000.hdf5",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
     ],
     setImageChannel: [
         {
@@ -175,6 +175,142 @@ let assertItem: AssertItem = {
     setRegion: [
         {
             controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 800, y: 800 }, { x: 400, y: 400 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
+            fileId: 0,
+            regionId: -1,
+            regionType: 3,
+            regionName: "",
+            rotation: 0,
+        },
+        {
+            controlPoints: [{ x: 1600, y: 1600 }, { x: 800, y: 800 }],
             fileId: 0,
             regionId: -1,
             regionType: 3,
@@ -209,14 +345,14 @@ describe("PERF_REGION_SPECTRAL_PROFILE", () => {
             }, connectTimeout);
 
             describe(`open the file "${fileOpen.directory}/${assertItem.fileOpen[index].file}"`, () => {
-                test(`OPEN_FILE_ACK and REGION_HISTOGRAM_DATA should arrive within ${openFileTimeout} ms`, async () => {
+                test(`(Step 1)"${assertItem.fileOpen[index].file}" OPEN_FILE_ACK and REGION_HISTOGRAM_DATA should arrive within ${openFileTimeout} ms`, async () => {
                     await Connection.send(CARTA.OpenFile, fileOpen);
                     await Connection.receiveAny()
                     await Connection.receiveAny() // OpenFileAck | RegionHistogramData
                 }, openFileTimeout);
 
                 let ack: AckStream;
-                test(`RASTER_TILE_DATA should arrive within ${readFileTimeout} ms`, async () => {
+                test(`(Step 1)"${assertItem.fileOpen[index].file}" SetImageChannels & SetCursor responses should arrive within ${readFileTimeout} ms`, async () => {
                     await Connection.send(CARTA.SetImageChannels, assertItem.setImageChannel[0]);
                     await Connection.send(CARTA.SetCursor, assertItem.setCursor[0]);
                     // await Connection.stream(assertItem.setImageChannel[0].requiredTiles.tiles.length);
@@ -224,17 +360,19 @@ describe("PERF_REGION_SPECTRAL_PROFILE", () => {
                     // console.log(ack)
                 }, readFileTimeout);
 
-                test(`SET_REGION_ACK should arrive within ${readRegionTimeout} ms`, async () => {
-                    await Connection.send(CARTA.SetRegion, assertItem.setRegion[0]);
+                test(`(Step 2)"${assertItem.fileOpen[index].file}" SET_REGION_ACK should arrive within ${readRegionTimeout} ms`, async () => {
+                    await Connection.send(CARTA.SetRegion, assertItem.setRegion[index]);
                     await Connection.receive(CARTA.SetRegionAck);
                 }, readRegionTimeout);
 
                 let ack3: AckStream;
-                test(`SPECTRAL_PROFILE_DATA stream should arrive within ${readFileTimeout} ms`, async () => {
+                test(`(Step 3)"${assertItem.fileOpen[index].file}" SPECTRAL_PROFILE_DATA stream should arrive within ${readFileTimeout} ms`, async () => {
                     await Connection.send(CARTA.SetSpectralRequirements, assertItem.setSpectralRequirements[0]);
 
                     let SpectralProfileDataTemp = await Connection.receive(CARTA.SpectralProfileData);
                     let ReceiveProgress: number = SpectralProfileDataTemp.progress;
+                    console.warn('' + assertItem.fileOpen[index].file + ' SpectralProfileData progress :', ReceiveProgress)
+
 
                     if (ReceiveProgress != 1) {
                         while (ReceiveProgress < 1) {
