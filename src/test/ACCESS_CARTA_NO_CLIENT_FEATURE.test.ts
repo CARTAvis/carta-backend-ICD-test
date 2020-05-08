f48b31242a10b1c20405cb0597a93eb88dc6a1ab
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2020-05-08 15:22:25 +0800

    Add test ACCESS_CARTA_NO_CLIENT_FEATURE

---
+++ src/test/ACCESS_CARTA_NO_CLIENT_FEATURE.test.ts
@@ -0,0 +1,64 @@
+import { CARTA } from "carta-protobuf";
+
+import { Client } from "./CLIENT";
+import config from "./config.json";
+let testServerUrl = config.serverURL;
+let connectTimeout = config.timeout.connection;
+interface AssertItem {
+    register: CARTA.IRegisterViewer;
+}
+let assertItem: AssertItem = {
+    register: {
+        sessionId: 0,
+        apiKey: "",
+        clientFeatureFlags: 0,
+    },
+}
+describe("ACCESS_CARTA_NO_CLIENT_FEATURE tests: Testing backend connection without any client feature", () => {
+    describe(`send "REGISTER_VIEWER" to "${testServerUrl}" with session_id=${assertItem.register.sessionId} & api_key="${assertItem.register.apiKey} & client_feature_flags="${assertItem.register.clientFeatureFlags}"`, () => {
+        let Connection: Client;
+        let RegisterViewerAckTemp: CARTA.RegisterViewerAck; 
+        test(`should get "REGISTER_VIEWER_ACK" within ${connectTimeout} ms`, async () => {
+            Connection = new Client(testServerUrl);
+            expect(Connection.connection.readyState).toBe(WebSocket.CONNECTING);
+
+            await Connection.open().then( () => {
+                expect(Connection.connection.readyState).toBe(WebSocket.OPEN);
+            });
+
+            await Connection.send(CARTA.RegisterViewer, assertItem.register);
+            RegisterViewerAckTemp = await Connection.receive(CARTA.RegisterViewerAck) as CARTA.RegisterViewerAck;
+
+            await Connection.close().then( () => {
+                expect(Connection.connection.readyState).toBe(WebSocket.CLOSED);
+            });
+
+        }, connectTimeout);
+
+        test("REGISTER_VIEWER_ACK.success = True", () => {
+            expect(RegisterViewerAckTemp.success).toBe(true);
+        });
+
+        test("REGISTER_VIEWER_ACK.session_id is non-empty string", () => {
+            expect(RegisterViewerAckTemp.sessionId).toBeDefined();
+            console.log(`Registered session ID is ${RegisterViewerAckTemp.sessionId} @${new Date()}`);
+        });
+
+        test(`REGISTER_VIEWER_ACK.session_type = "CARTA.SessionType.NEW"`, () => {
+            expect(RegisterViewerAckTemp.hasOwnProperty("sessionType")).toBe(true);
+            expect(RegisterViewerAckTemp.sessionType).toBe(CARTA.SessionType.NEW);
+        });
+
+        test("REGISTER_VIEWER_ACK.server_feature_flags = 8", () => {
+            expect(RegisterViewerAckTemp.serverFeatureFlags).toEqual(8);
+        });
+
+        test("REGISTER_VIEWER_ACK.user_preferences = None", () => {
+            expect(RegisterViewerAckTemp.userPreferences).toMatchObject({});
+        });
+
+        test("REGISTER_VIEWER_ACK.user_layouts = None", () => {
+            expect(RegisterViewerAckTemp.userLayouts).toMatchObject({});
+        });
+    });
+});
