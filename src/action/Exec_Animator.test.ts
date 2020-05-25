717ae731257f0e96e42037606167c2fd04beeda6
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2020-05-25 15:24:41 +0800

    Auto exec on action for animator

---
+++ src/action/Exec_Animator.test.ts
@@ -0,0 +1,169 @@
+import { CARTA } from "carta-protobuf";
+
+import Long from "long";
+
+import { Client, AckStream } from "./CLIENT";
+import * as SocketOperation from "./SocketOperation";
+import config from "./config.json";
+let testServerUrl: string = config.serverURL;
+let testSubdirectory: string = config.path.performance;
+let execTimeout: number = config.timeout.execute;
+let connectTimeout: number = config.timeout.connection;
+let animatorTimeout: number = config.timeout.playAnimator;
+let animatorFlame: number = config.repeat.animation;
+interface AssertItem {
+    register: CARTA.IRegisterViewer;
+    filelist: CARTA.IFileListRequest;
+    fileOpenGroup: CARTA.IOpenFile[];
+    setCursor: CARTA.ISetCursor;
+    addTilesReq: CARTA.IAddRequiredTiles;
+    startAnimation: CARTA.IStartAnimation;
+    stopAnimation: CARTA.IStopAnimation;
+}
+let assertItem: AssertItem = {
+    register: {
+        sessionId: 0,
+        apiKey: "",
+        clientFeatureFlags: 5,
+    },
+    filelist: { directory: testSubdirectory },
+    fileOpenGroup: [
+        {
+            directory: testSubdirectory,
+            file: "cube_A/cube_A_00800_z00100.fits",
+            hdu: "",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        },
+    ],
+    setCursor: {
+        fileId: 0,
+        point: { x: 1.0, y: 1.0 },
+        spatialRequirements: {
+            fileId: 0,
+            regionId: 0,
+            spatialProfiles: []
+        },
+    },
+    addTilesReq:
+    {
+        tiles: [0],
+        fileId: 0,
+        compressionQuality: 11,
+        compressionType: CARTA.CompressionType.ZFP,
+    },
+    startAnimation: {
+        fileId: 0,
+        startFrame: { channel: 1, stokes: 0 },
+        firstFrame: { channel: 0, stokes: 0 },
+        lastFrame: { channel: animatorFlame, stokes: 0 },
+        deltaFrame: { channel: 1, stokes: 0 },
+        requiredTiles: {
+            fileId: 0,
+            tiles: [50343939, 50343938, 50339843, 50339842, 50348035,
+                50343940, 50348034, 50339844, 50343937, 50335747,
+                50339841, 50335746, 50348036, 50348033, 50335748,
+                50335745, 50352131, 50343941, 50352130, 50339845,
+                50343936, 50331651, 50339840, 50331650, 50352132,
+                50348037, 50352129, 50335749, 50348032, 50331652,
+                50335744, 50331649, 50352133, 50356227, 50343942,
+                50356226, 50339846, 50352128, 50331653, 50356228,
+                50348038, 50331648, 50356225, 50335750, 50356229,
+                50352134, 50356224, 50331654, 50356230],
+            compressionType: CARTA.CompressionType.ZFP,
+            compressionQuality: 9,
+        },
+        looping: false,
+        reverse: false,
+        frameRate: 30,
+    },
+    stopAnimation: {
+        fileId: 0,
+        endFrame: { channel: animatorFlame, stokes: 0 },
+    },
+}
+
+describe("Animator action: ", () => {
+    let Connection: Client;
+    let cartaBackend: any;
+    beforeAll(async () => {
+        cartaBackend = await SocketOperation.CartaBackend(
+                "/almalustre/carta/images", // baseDirectory
+                5000, // port
+                4, // threadNumber
+                32, // ompThreadNumber
+            );
+
+    }, execTimeout);
+
+
+    describe(`Start the action: animator`, () => {
+        beforeAll(async () => {
+            Connection = new Client(testServerUrl);
+            await Connection.open();
+            await Connection.send(CARTA.RegisterViewer, assertItem.register);
+            await Connection.receive(CARTA.RegisterViewerAck);
+            await Connection.send(CARTA.FileListRequest, assertItem.filelist);
+            await Connection.receive(CARTA.FileListResponse);
+        }, connectTimeout);
+
+        assertItem.fileOpenGroup.map((fileOpen: CARTA.IOpenFile, index) => {
+
+            describe(`open the file "${fileOpen.file}"`, () => {
+                test(`should play animator with ${assertItem.stopAnimation.endFrame.channel} frames`, async () => {
+                    let ackStream: AckStream;
+                    await Connection.send(CARTA.OpenFile, fileOpen);
+                    await Connection.receiveAny(); // OpenFileAck
+
+                    await Connection.send(CARTA.AddRequiredTiles, assertItem.addTilesReq);
+                    await Connection.send(CARTA.SetCursor, assertItem.setCursor);
+                    while (true) {
+                        ackStream = await Connection.stream(1) as AckStream;
+                        if (ackStream.RasterTileSync.length > 0) {
+                            if (ackStream.RasterTileSync[0].endSync) {
+                                break;
+                            }
+                        }
+                    }
+
+                    await Connection.send(CARTA.StartAnimation, assertItem.startAnimation);
+                    await Connection.send(CARTA.AddRequiredTiles, assertItem.startAnimation.requiredTiles);
+                    await Connection.receive(CARTA.StartAnimationAck);
+
+                    for (let channel: number = 1; channel <= assertItem.stopAnimation.endFrame.channel; channel++) {
+                        while (true) {
+                            ackStream = await Connection.stream(1) as AckStream;
+                            if (ackStream.RasterTileSync.length > 0) {
+                                if (ackStream.RasterTileSync[0].endSync) {
+                                    break;
+                                }
+                            }
+                        };
+                        await Connection.send(CARTA.AnimationFlowControl,
+                            {
+                                fileId: 0,
+                                animationId: 1,
+                                receivedFrame: {
+                                    channel: channel,
+                                    stokes: 0
+                                },
+                                timestamp: Long.fromNumber(Date.now()),
+                            }
+                        );
+                    }
+                    await Connection.send(CARTA.StopAnimation, assertItem.stopAnimation);
+
+                    await new Promise(resolve => setTimeout(resolve, 300));
+                    await Connection.send(CARTA.CloseFile, { fileId: -1 });
+                }, animatorTimeout);
+            });
+
+        });
+    });
+
+    afterAll(async done => {
+        await Connection.close();
+        await cartaBackend.kill();
+        cartaBackend.on("close", () => done());
+    });
+});
---
+++ src/action/SocketOperation.ts
@@ -0,0 +1,428 @@
+import * as child_process from "child_process";
+
+import { CARTA } from "carta-protobuf";
+
+import * as Utility from "../UtilityFunction";
+let nodeusage = require("usage");
+let procfs = require("procfs-stats");
+let fs = require("fs");
+
+/// Log result
+export interface Report {
+    file: string;
+    timeEpoch: TimeEpoch[];
+}
+export interface TimeEpoch {
+    time: number;
+    thread: number;
+    CPUusage: number;
+    RAM: number;
+    fileName: string;
+    Disk: number;
+}
+export async function
+    Outcome(
+        timeEpoch: { time: number, thread: number, CPUusage: number, RAM: number }[],
+) {
+    console.log(`Backend testing outcome:\n${timeEpoch
+        .map(e => `${e.time.toPrecision(5)}ms with CPU usage = ${e.CPUusage.toPrecision(5)}% & RAM = ${e.RAM}kB as thread# = ${e.thread}`).join(` \n`)}`);
+}
+export async function
+    OutcomeWithFile(
+        timeEpoch: { time: number, thread: number, CPUusage: number, RAM: number, fileName: string }[],
+) {
+    console.log(`Backend testing outcome:\n${timeEpoch
+        .map(e => `${e.time.toPrecision(5)}ms with CPU usage = ${e.CPUusage.toPrecision(5)}% & RAM = ${e.RAM}kB as file: ${e.fileName}`).join(` \n`)}`);
+}
+export async function
+    OutcomeWithDiskIO(
+        timeEpoch: { time: number, thread: number, CPUusage: number, RAM: number, fileName: string, Disk: number }[],
+) {
+    console.log(`Backend testing outcome:\n${timeEpoch
+        .map(e => `${e.time.toPrecision(5)}ms with CPU usage = ${e.CPUusage.toPrecision(5)}% & RAM = ${e.RAM / 1024}kB & Disk read = ${(e.Disk / 1024).toFixed(0)}kB as file: ${e.fileName}`).join(` \n`)}`);
+}
+export async function
+    Report(
+        report: Report,
+) {
+    console.log(`Backend testing outcome on ${report.file}:\n${report.timeEpoch
+        .map(e => `${e.time.toPrecision(5)}ms with CPU usage = ${e.CPUusage.toPrecision(5)}% & RAM = ${e.RAM}kB as thread# = ${e.thread}`).join(` \n`)}`);
+}
+/// Record label to file
+export async function
+    WriteLebelTo(
+        reportFileName: string,
+) {
+    fs.appendFileSync(reportFileName,
+        `ImageFile\t#ThreadSet\tTimeElapsed\tCPU\tRAM\tDisk\t#ThreadReal\n`
+    );
+}
+/// Record result to file
+export async function
+    WriteReportTo(
+        reportFileName: string,
+        pid: number,
+        imageFile: string,
+        threadNumberSet: number,
+        timeElapsed: number[],
+        cpuCount: { user: number, total: number } = { user: 0, total: 0 },
+) {
+    let diskR: number = 0;
+    let usedThreadNumber: number = 0;
+    if (procfs.works) {
+        let ps = procfs(pid);
+        await new Promise(resolve => {
+            ps.io((err, io) => {
+                diskR = io.read_bytes;
+                resolve();
+            });
+        });
+        await new Promise(resolve => {
+            ps.threads((err, task) => {
+                usedThreadNumber = task.length;
+                resolve();
+            });
+        });
+    }
+    let cpuUsage = cpuCount.total ? (100 * cpuCount.user / cpuCount.total).toFixed(4) : 0;
+    await new Promise(resolve => {
+        nodeusage.lookup(
+            pid,
+            (err, info) => {
+                fs.appendFileSync(reportFileName,
+                    `${imageFile.split("/")[1].slice(7)}\t` +
+                    `${threadNumberSet.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })}\t` +
+                    `${(timeElapsed.reduce((a, b) => a + b) / timeElapsed.length).toFixed(4)}\t` +
+                    `${cpuUsage ? cpuUsage : info.cpu.toFixed(4)}\t` +
+                    `${info.memory}\t` +
+                    `${diskR}\t` +
+                    `${usedThreadNumber.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })}\t` +
+                    "\n"
+                );
+                resolve();
+            }
+        );
+    });
+}
+/// Create a new carta_backend service
+export async function
+    CartaBackend(
+        baseDirectory: string,
+        port: number,
+        threadNumber: number,
+        ompThreadNumber: number,
+        backendDirectory?: string,
+        outputFile?: string,
+        timeout?: number,
+        logMessage?: boolean,
+    ) {
+    let cartaBackend = await child_process.execFile(
+        `/usr/local/bin/pagecache-management.sh /home/hengtai/carta-backend/build/carta_backend`,
+        [
+            `root=base`, `base=${baseDirectory}`,
+            `port=${port}`, `threads=${threadNumber}`,
+            `verbose=true`, `omp_threads=${ompThreadNumber}`,
+            outputFile ? `> ${outputFile}` : "",
+        ],
+        {
+            // cwd: backendDirectory, 
+            timeout
+        }
+    );
+    cartaBackend.on("error", error => {
+        console.error(`error: \n ${error}`);
+    });
+    cartaBackend.stdout.on("data", data => {
+        if (logMessage) {
+            console.log(data);
+        }
+    });
+    return cartaBackend;
+}
+/// Create a psrecord monitor
+export async function
+    Psrecord(
+        pid: number,
+        saveDirectory: string,
+        fileName: string,
+        threadNumber: number,
+        timeout: number,
+) {
+    let psrecord = await child_process.exec(
+        `psrecord` + ` ${pid}` +
+        ` --log ${fileName.split("/")[1].slice(7)}-${threadNumber.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })}.txt` +
+        ` --plot ${fileName.split("/")[1].slice(7)}-${threadNumber.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false })}.png` +
+        ` --interval 0.005`,
+        {
+            cwd: saveDirectory,
+            timeout
+        }
+    );
+    return psrecord;
+}
+/// Create a new client connection
+export async function
+    SocketClient(
+        serverURL: string,
+        port: number,
+        reconnectWait: number,
+) {
+    let Connection = await new WebSocket(`${serverURL}:${port}`);
+
+    while (Connection.readyState !== WebSocket.OPEN) {
+        await Connection.close();
+        Connection = await new WebSocket(`${serverURL}:${port}`);
+        await new Promise(time => setTimeout(time, reconnectWait));
+    }
+    Connection.binaryType = "arraybuffer";
+
+    return Connection;
+}
+/// Send CARTA.RegisterViewer then get CARTA.RegisterViewerAck
+export async function
+    RegisterViewer(
+        Connection: WebSocket,
+        CallbackFunc: () => Promise<void> = undefined,
+) {
+    let RegisterViewerAckTemp: CARTA.RegisterViewerAck;
+    await Utility.setEvent(Connection, CARTA.RegisterViewer,
+        {
+            sessionId: 0,
+            apiKey: "1234"
+        }
+    );
+    await new Promise(resolve => {
+        Utility.getEvent(Connection, CARTA.RegisterViewerAck,
+            async RegisterViewerAck => {
+                expect(RegisterViewerAck.success).toBe(true);
+                if (CallbackFunc) {
+                    await CallbackFunc();
+                }
+                resolve();
+            }
+        );
+    });
+    return RegisterViewerAckTemp;
+}
+/// Send CARTA.OpenFile then get CARTA.OpenFileAck
+export async function
+    OpenFile(
+        Connection: WebSocket,
+        directory: string,
+        file: string,
+        CallbackFunc: (timer: number) => Promise<void> = undefined,
+) {
+    let OpenFileAckTemp: CARTA.OpenFileAck;
+    await Utility.setEvent(Connection, CARTA.OpenFile,
+        {
+            directory,
+            file,
+            hdu: "0",
+            fileId: 0,
+            renderMode: CARTA.RenderMode.RASTER,
+        }
+    );
+    let timer = await performance.now();
+    await new Promise(resolve => {
+        Utility.getEvent(Connection, CARTA.OpenFileAck,
+            async (OpenFileAck: CARTA.OpenFileAck) => {
+                if (!OpenFileAck.success) {
+                    console.error(OpenFileAck.fileInfo.name + " : " + OpenFileAck.message);
+                }
+                expect(OpenFileAck.success).toBe(true);
+                OpenFileAckTemp = OpenFileAck;
+                if (CallbackFunc) {
+                    await CallbackFunc(timer);
+                }
+                resolve();
+            }
+        );
+    });
+    return OpenFileAckTemp;
+}
+/// Send CARTA.SetImageView then get CARTA.RasterImageData
+export async function
+    SetImageView(
+        Connection: WebSocket,
+        OpenFileAck: CARTA.OpenFileAck,
+        CallbackFunc: (timer: number) => Promise<void> = undefined,
+) {
+    let RasterImageDataTemp: CARTA.RasterImageData;
+    await Utility.setEvent(Connection, CARTA.SetImageView,
+        {
+            fileId: 0,
+            imageBounds: {
+                xMin: 0, xMax: OpenFileAck.fileInfoExtended.width,
+                yMin: 0, yMax: OpenFileAck.fileInfoExtended.height
+            },
+            mip: 16,
+            compressionType: CARTA.CompressionType.ZFP,
+            compressionQuality: 11,
+            numSubsets: 4,
+        }
+    );
+    let timer = await performance.now();
+    await new Promise(resolve => {
+        Utility.getEvent(Connection, CARTA.RasterImageData,
+            async (RasterImageData: CARTA.RasterImageData) => {
+                expect(RasterImageData.fileId).toEqual(0);
+                RasterImageDataTemp = RasterImageData;
+                if (CallbackFunc) {
+                    await CallbackFunc(timer);
+                }
+                resolve();
+            }
+        );
+    });
+    return RasterImageDataTemp;
+}
+/// Send CARTA.SetImageView & CARTA.SetSpatialRequirements then get CARTA.RasterImageData
+export async function
+    SetSpatialRequirements(
+        Connection: WebSocket,
+        OpenFileAck: CARTA.OpenFileAck,
+        CallbackFunc: (timer: number) => Promise<void> = undefined,
+) {
+    let RasterImageDataTemp: CARTA.RasterImageData;
+    await Utility.setEvent(Connection, CARTA.SetImageView,
+        {
+            fileId: 0,
+            imageBounds: {
+                xMin: 0, xMax: OpenFileAck.fileInfoExtended.width,
+                yMin: 0, yMax: OpenFileAck.fileInfoExtended.height
+            },
+            mip: 16,
+            compressionType: CARTA.CompressionType.ZFP,
+            compressionQuality: 11,
+            numSubsets: 4,
+        }
+    );
+    await Utility.setEvent(Connection, CARTA.SetSpatialRequirements,
+        {
+            fileId: 0,
+            regionId: 0,
+            spatialProfiles: ["x", "y"],
+        }
+    );
+    let timer = await performance.now();
+    await new Promise(resolve => {
+        Utility.getEvent(Connection, CARTA.RasterImageData,
+            async (RasterImageData: CARTA.RasterImageData) => {
+                expect(RasterImageData.fileId).toEqual(0);
+                RasterImageDataTemp = RasterImageData;
+                if (CallbackFunc) {
+                    await CallbackFunc(timer);
+                }
+                resolve();
+            }
+        );
+    });
+    return RasterImageDataTemp;
+}
+/// Send CARTA.SetCursor then get CARTA.SpatialProfileData
+export async function
+    CursorSpatialProfileData(
+        Connection: WebSocket,
+        RasterImageData: CARTA.RasterImageData,
+        CallbackFunc: (timer: number) => Promise<void> = undefined,
+) {
+    let SpatialProfileDataTemp: CARTA.SpatialProfileData;
+    await Utility.setEvent(Connection, CARTA.SetCursor,
+        {
+            fileId: 0,
+            point: {
+                x: Math.floor(Math.random() * RasterImageData.imageBounds.xMax),
+                y: Math.floor(Math.random() * RasterImageData.imageBounds.yMax)
+            },
+        }
+    );
+    let timer = await performance.now();
+    await new Promise(resolve => {
+        Utility.getEvent(Connection, CARTA.SpatialProfileData,
+            async (SpatialProfileData: CARTA.SpatialProfileData) => {
+                expect(SpatialProfileData.profiles.length).not.toEqual(0);
+                SpatialProfileDataTemp = SpatialProfileData;
+                if (CallbackFunc) {
+                    await CallbackFunc(timer);
+                }
+                resolve();
+            }
+        );
+    });
+    return SpatialProfileDataTemp;
+}
+/// Send CARTA.SetImageView & CARTA.SetSpectralRequirements then get CARTA.RasterImageData
+export async function
+    SetSpectralRequirements(
+        Connection: WebSocket,
+        OpenFileAck: CARTA.OpenFileAck,
+        CallbackFunc: (timer: number) => Promise<void> = undefined,
+) {
+    let RasterImageDataTemp: CARTA.RasterImageData;
+    await Utility.setEvent(Connection, CARTA.SetImageView,
+        {
+            fileId: 0,
+            imageBounds: {
+                xMin: 0, xMax: OpenFileAck.fileInfoExtended.width,
+                yMin: 0, yMax: OpenFileAck.fileInfoExtended.height
+            },
+            mip: 16,
+            compressionType: CARTA.CompressionType.ZFP,
+            compressionQuality: 11,
+            numSubsets: 4,
+        }
+    );
+    await Utility.setEvent(Connection, CARTA.SetSpectralRequirements,
+        {
+            fileId: 0,
+            regionId: 0,
+            spectralProfiles: [{ coordinate: "z", statsTypes: [CARTA.StatsType.None] }],
+        }
+    );
+    let timer = await performance.now();
+    await new Promise(resolve => {
+        Utility.getEvent(Connection, CARTA.RasterImageData,
+            async (RasterImageData: CARTA.RasterImageData) => {
+                expect(RasterImageData.fileId).toEqual(0);
+                RasterImageDataTemp = RasterImageData;
+                if (CallbackFunc) {
+                    await CallbackFunc(timer);
+                }
+                resolve();
+            }
+        );
+    });
+    return RasterImageDataTemp;
+}
+/// Send CARTA.SetCursor then get CARTA.SpectralProfileData
+export async function
+    CursorSpectralProfileData(
+        Connection: WebSocket,
+        RasterImageData: CARTA.RasterImageData,
+        CallbackFunc: (timer: number) => Promise<void> = undefined,
+) {
+    let SpectralProfileDataTemp: CARTA.SpectralProfileData;
+    await Utility.setEvent(Connection, CARTA.SetCursor,
+        {
+            fileId: 0,
+            point: {
+                x: Math.floor(Math.random() * RasterImageData.imageBounds.xMax),
+                y: Math.floor(Math.random() * RasterImageData.imageBounds.yMax)
+            },
+        }
+    );
+    let timer = await performance.now();
+    await new Promise(resolve => {
+        Utility.getEvent(Connection, CARTA.SpectralProfileData,
+            async (SpectralProfileData: CARTA.SpectralProfileData) => {
+                expect(SpectralProfileData.profiles.length).not.toEqual(0);
+                SpectralProfileDataTemp = SpectralProfileData;
+                if (CallbackFunc) {
+                    await CallbackFunc(timer);
+                }
+                resolve();
+            }
+        );
+    });
+    return SpectralProfileDataTemp;
+}
