af50eb3c6857e7be718c54e69c34e651441d10dc
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2020-05-29 17:50:22 +0800

    Add exec action for contour

--- src/action/Exec_Contour.test.ts
+++ src/action/Exec_Contour.test.ts
@@ -1,10 +1,12 @@
 import { CARTA } from "carta-protobuf";
 
 import { Client, AckStream } from "./CLIENT";
+import * as Socket from "./SocketOperation";
 import config from "./config.json";
-let testServerUrl: string = config.serverURL;
-let testImage: string = config.image.cube;
+let testServerUrl: string = config.localHost + ":" + config.port;
 let testSubdirectory: string = config.path.performance;
+let testImage: string = config.image.singleChannel;
+let execTimeout: number = config.timeout.execute;
 let connectTimeout: number = config.timeout.connection;
 let openfileTimeout: number = config.timeout.openFile;
 let contourTimeout: number = config.timeout.contour;
@@ -52,11 +54,11 @@ let assertItem: AssertItem = {
         referenceFileId: 0,
         imageBounds: { xMin: 0, xMax: 800, yMin: 0, yMax: 800 },
         levels: [
-            0.026764668757095933, 
-            2.817174522397773, 
-            5.60758437603845, 
-            8.397994229679128, 
-            11.188404083319805
+            0.015808638865844493,
+            2.810852870937488,
+            5.605897103009132,
+            8.400941335080777,
+            11.19598556715242,
         ],
         smoothingMode: CARTA.SmoothingMode.GaussianBlur,
         smoothingFactor: 4,
@@ -68,18 +70,22 @@ let assertItem: AssertItem = {
 
 describe("Contour action: ", () => {
     let Connection: Client;
-    beforeAll(async () => {
-        Connection = new Client(testServerUrl);
-        await Connection.open();
-        await Connection.send(CARTA.RegisterViewer, assertItem.register);
-        await Connection.receive(CARTA.RegisterViewerAck);
-    }, connectTimeout);
+    let cartaBackend: any;
+    let logFile = assertItem.fileOpen.file.substr(assertItem.fileOpen.file.search('/') + 1).replace('.', '_') + "_contour.txt";
+    test(`CARTA is ready`, async () => {
+        cartaBackend = await Socket.CartaBackend(
+            logFile,
+            config.port,
+        );
+        await new Promise(resolve => setTimeout(resolve, config.wait.exec));
+    }, execTimeout);
 
-
-    describe(`Go to "${assertItem.filelist.directory}" folder`, () => {
-        beforeAll(async () => {
-            await Connection.send(CARTA.FileListRequest, assertItem.filelist);
-            await Connection.receive(CARTA.FileListResponse);
+    describe(`Start the action: contour`, () => {
+        test(`Connection is ready`, async () => {
+            Connection = new Client(testServerUrl);
+            await Connection.open();
+            await Connection.send(CARTA.RegisterViewer, assertItem.register);
+            await Connection.receive(CARTA.RegisterViewerAck);
         }, connectTimeout);
 
         describe(`open the file "${assertItem.fileOpen.file}"`, () => {
@@ -103,12 +109,12 @@ describe("Contour action: ", () => {
                         },
                         ...assertItem.setContour,
                     });
-                    let contourImageData = await Connection.receive(CARTA.ContourImageData);
+                    let contourImageData: CARTA.ContourImageData = await Connection.receive(CARTA.ContourImageData) as CARTA.ContourImageData;
                     let count: number = 0;
                     if (contourImageData.progress == 1) count++;
 
                     while (count < assertItem.setContour.levels.length) {
-                        contourImageData = await Connection.receive(CARTA.ContourImageData);
+                        contourImageData = await Connection.receive(CARTA.ContourImageData) as CARTA.ContourImageData;
                         if (contourImageData.progress == 1) count++;
                     }
                     await Connection.send(CARTA.SetContourParameters, {
@@ -116,13 +122,16 @@ describe("Contour action: ", () => {
                         referenceFileId: 0,
                     }); // Clear contour
 
-                    await new Promise(resolve => setTimeout(resolve, 200));
                 }, contourTimeout);
             }
-            
+
         });
 
     });
 
-    afterAll(() => Connection.close());
+    afterAll(async done => {
+        await Connection.close();
+        cartaBackend.kill();
+        cartaBackend.on("close", () => done());
+    }, execTimeout);
 });
