b72b8f3e1549e57b1fa18ef925283e85c647e429
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2020-06-02 13:59:42 +0800

    Add exec action for zoom in on many images

--- src/action/Exec_ZoomInImages.test.ts
+++ src/action/Exec_ZoomInImages.test.ts
@@ -55,10 +55,22 @@ let assertItem: AssertItem = {
             compressionType: CARTA.CompressionType.ZFP,
         },
         {
-            tiles: [33558529, 33562625, 33558530, 33558528, 33554433,
-                33562626, 33562624, 33554434, 33554432, 33566721,
-                33558531, 33566722, 33562627, 33566720, 33554435,
-                33566723],
+            tiles: [67133446, 67133445, 67129350, 67129349, 67137542,
+                67133447, 67137541, 67129351, 67137543, 67133444,
+                67125254, 67129348, 67125253, 67137540, 67125255,
+                67141638, 67133448, 67141637, 67129352, 67125252,
+                67141639, 67137544, 67133443, 67121158, 67141636,
+                67125256, 67129347, 67121157, 67137539, 67121159,
+                67141640, 67145734, 67133449, 67125251, 67121156,
+                67145733, 67129353, 67145735, 67137545, 67141635,
+                67121160, 67145732, 67125257, 67133442, 67129346,
+                67121155, 67145736, 67141641, 67137538, 67125250,
+                67133450, 67145731, 67121161, 67129354, 67141634,
+                67137546, 67145737, 67125258, 67121154, 67133441,
+                67141642, 67129345, 67137537, 67145730, 67121162,
+                67125249, 67133451, 67141633, 67129355, 67145738,
+                67137547, 67121153, 67125259, 67141643, 67145729,
+                67121163, 67145739],
             fileId: 0,
             compressionQuality: 11,
             compressionType: CARTA.CompressionType.ZFP,
@@ -86,10 +98,10 @@ let testFiles = [
 ];
 testFiles.map(file => {
     let testServerUrl: string = `${config.localHost}:${config.port}`;
-    describe(`Zoom In&Out Iamge action: ${file.substr(file.search('/') + 1)}`, () => {
+    describe(`Zoom In Iamge action: ${file.substr(file.search('/') + 1)}`, () => {
         let Connection: Client;
         let cartaBackend: any;
-        let logFile = file.substr(file.search('/') + 1).replace('.', '_') + "_ZoomOut.txt";
+        let logFile = file.substr(file.search('/') + 1).replace('.', '_') + "_ZoomIn.txt";
         test(`CARTA is ready`, async () => {
             cartaBackend = await Socket.CartaBackend(
                 logFile,
@@ -127,7 +139,7 @@ testFiles.map(file => {
                 }, readTimeout);;
 
                 for (let idx: number = 0; idx < zoomRepeat; idx++) {
-                    test(`image should zoom out`, async () => {
+                    test(`image should zoom in`, async () => {
                         await Connection.send(CARTA.AddRequiredTiles, assertItem.addTilesReq[1]);
                         let ack: AckStream;
                         while (true) {
@@ -141,7 +153,7 @@ testFiles.map(file => {
                         await new Promise(resolve => setTimeout(resolve, config.wait.zoom));
                     }, zoomTimeout);
 
-                    test(`image should zoom in`, async () => {
+                    test(`image should zoom out`, async () => {
                         await Connection.send(CARTA.AddRequiredTiles, assertItem.addTilesReq[0]);
                         let ack: AckStream;
                         while (true) {
