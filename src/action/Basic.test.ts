7909769a04c12d46b7177f0f08c6ad2d97759706
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2020-08-05 10:58:32 +0800

    Start to use node-usage lib

---
+++ src/action/Basic.test.ts
@@ -0,0 +1,43 @@
+
+let nodeusage = require("usage");
+function updateUsage(t, timeout, step, done) {
+    setTimeout(() => {
+        nodeusage.lookup(
+            process.pid,
+            (err, info) => {
+                console.log(info);
+                if (t < timeout) {
+                    updateUsage(t + step, timeout, done);
+                } else {
+                    setTimeout(() => {
+                        nodeusage.clearHistory(); //clean history for all pids
+                    }, step);
+                    done();
+                }
+            }
+        );
+    }, step);
+}
+describe(`node-usage test`, () => {
+    test(`should read CPU usage`,
+        done => {
+            nodeusage.lookup(
+                process.pid,
+                (err, info) => {
+                    expect(info.cpu).toBeGreaterThan(0);
+                    updateUsage(0, 400, 100, done);
+                }
+            );
+        });
+
+    test(`should read RAM usage`,
+        done => {
+            nodeusage.lookup(
+                process.pid,
+                (err, info) => {
+                    expect(info.memory).toBeGreaterThan(0);
+                    done();
+                }
+            );
+        });
+});
