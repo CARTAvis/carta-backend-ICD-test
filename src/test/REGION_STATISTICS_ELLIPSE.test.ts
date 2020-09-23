03b74012ff67d9770813e1f2b8d4fc7562c04421
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2020-09-23 10:52:33 +0800

    Update test of PER_ and REGION_

--- src/test/REGION_STATISTICS_ELLIPSE.test.ts
+++ src/test/REGION_STATISTICS_ELLIPSE.test.ts
@@ -1,6 +1,8 @@
 import { CARTA } from "carta-protobuf";
+
 import { Client, AckStream } from "./CLIENT";
 import config from "./config.json";
+var W3CWebSocket = require('websocket').w3cwebsocket;
 
 let testServerUrl = config.serverURL;
 let testSubdirectory = config.path.QA;
@@ -230,7 +232,7 @@ describe("REGION_STATISTICS_RECTANGLE test: Testing statistics with rectangle re
     }, connectTimeout);
 
     test(`(Step 0) Connection open? | `, () => {
-        expect(Connection.connection.readyState).toBe(WebSocket.OPEN);
+        expect(Connection.connection.readyState).toBe(W3CWebSocket.OPEN);
     });
 
     assertItem.openFile.map(openFile => {
