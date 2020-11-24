e5c1c29309d4a8821b08360c7fb945b439a10740
acdo2002 <acdo2002@gmail.com>
2020-11-24 14:17:31 +0800

    finish CLOSE_FILE_SPECTRAL_PROFILE.test.ts

--- src/test/CLOSE_FILE_SPECTRAL_PROFILE.test.ts
+++ src/test/CLOSE_FILE_SPECTRAL_PROFILE.test.ts
@@ -12,10 +12,10 @@ let readFileTimeout: number = config.timeout.readFile;
 let largeImageTimeout = config.timeout.readLargeImage;
 
 interface AssertItem {
-    register: CARTA.IRegisterViewer;
+    registerViewer: CARTA.IRegisterViewer;
     filelist: CARTA.IFileListRequest;
-    fileOpen: CARTA.IOpenFile[];
-    addTilesReq: CARTA.IAddRequiredTiles[];
+    openFile: CARTA.IOpenFile[];
+    addRequiredTiles: CARTA.IAddRequiredTiles[];
     setCursor: CARTA.ISetCursor[];
     setSpatialReq: CARTA.ISetSpatialRequirements[];
     setRegion: CARTA.ISetRegion[];
@@ -24,12 +24,12 @@ interface AssertItem {
 };
 
 let assertItem: AssertItem = {
-    register: {
+    registerViewer: {
         sessionId: 0,
         clientFeatureFlags: 5,
     },
     filelist: { directory: testSubdirectory },
-    fileOpen: [
+    openFile: [
         {
             directory: testSubdirectory,
             file: "S255_IR_sci.spw29.cube.I.pbcor.fits",
@@ -45,7 +45,7 @@ let assertItem: AssertItem = {
             renderMode: CARTA.RenderMode.RASTER,
         },
     ],
-    addTilesReq: [
+    addRequiredTiles: [
         {
             fileId: 0,
             compressionQuality: 11,
@@ -145,8 +145,7 @@ describe("[Case 1] Request SPECTRAL_REQUIREMENTS and then CLOSE_FILE when data i
     beforeAll(async () => {
         Connection = new Client(testServerUrl);
         await Connection.open();
-        await Connection.send(CARTA.RegisterViewer, assertItem.register);
-        await Connection.receive(CARTA.RegisterViewerAck);
+        await Connection.registerViewer(assertItem.registerViewer);
     }, connectTimeout);
 
     test(`(Step 0) Connection open? | `, () => {
@@ -155,22 +154,19 @@ describe("[Case 1] Request SPECTRAL_REQUIREMENTS and then CLOSE_FILE when data i
 
     test(`(Step 1) OPEN_FILE_ACK and REGION_HISTOGRAM_DATA should arrive within ${openFileTimeout} ms`, async () => {
         await Connection.send(CARTA.CloseFile, { fileId: -1 });
-        await Connection.send(CARTA.CloseFile, { fileId: 0 });
-        await Connection.send(CARTA.OpenFile, assertItem.fileOpen[0]);
-        let OpenAck = await Connection.receive(CARTA.OpenFileAck)
-        await Connection.receive(CARTA.RegionHistogramData) // OpenFileAck | RegionHistogramData
-        expect(OpenAck.success).toBe(true)
-        expect(OpenAck.fileInfo.name).toEqual(assertItem.fileOpen[0].file)
+        let OpenAck = await Connection.openFile(assertItem.openFile[0]);
+        expect(OpenAck.OpenFileAck.success).toBe(true)
+        expect(OpenAck.OpenFileAck.fileInfo.name).toEqual(assertItem.openFile[0].file)
     }, openFileTimeout);
 
     let ack: AckStream;
     test(`(Step 2) return RASTER_TILE_DATA(Stream) and check total length `, async () => {
-        await Connection.send(CARTA.AddRequiredTiles, assertItem.addTilesReq[0]);
+        await Connection.send(CARTA.AddRequiredTiles, assertItem.addRequiredTiles[0]);
         await Connection.send(CARTA.SetCursor, assertItem.setCursor[0]);
         await Connection.send(CARTA.SetSpatialRequirements, assertItem.setSpatialReq[0]);
-        ack = await Connection.stream(5, 2500) as AckStream;
+        let ack = await Connection.streamUntil((type, data) => type == CARTA.RasterTileSync ? data.endSync : false);
         expect(ack.RasterTileSync.length).toEqual(2) //RasterTileSync: start & end
-        expect(ack.RasterTileData.length).toEqual(assertItem.addTilesReq[0].tiles.length) //only 1 Tile returned
+        expect(ack.RasterTileData.length).toEqual(assertItem.addRequiredTiles[0].tiles.length) //only 1 Tile returned
     }, readFileTimeout);
 
     let SetRegionAckTemp: CARTA.SetRegionAck;
@@ -191,7 +187,7 @@ describe("[Case 1] Request SPECTRAL_REQUIREMENTS and then CLOSE_FILE when data i
             while (ReceiveProgress <= 0.5) {
                 SpectralProfileDataTemp = await Connection.receive(CARTA.SpectralProfileData);
                 ReceiveProgress = SpectralProfileDataTemp.progress
-                console.warn('' + assertItem.fileOpen[0].file + ' SPECTRAL_PROFILE progress :', ReceiveProgress)
+                console.warn('' + assertItem.openFile[0].file + ' SPECTRAL_PROFILE progress :', ReceiveProgress)
             };
 
             //Once progress>0.5, then CLOSE_FILE
@@ -217,8 +213,7 @@ describe("[Case 2] Request SPECTRAL_REQUIREMENTS of TWO images and then CLOSE_FI
     beforeAll(async () => {
         Connection = new Client(testServerUrl);
         await Connection.open();
-        await Connection.send(CARTA.RegisterViewer, assertItem.register);
-        await Connection.receive(CARTA.RegisterViewerAck);
+        await Connection.registerViewer(assertItem.registerViewer);
     }, connectTimeout);
 
     test(`(Step 0) Connection open? | `, () => {
@@ -227,41 +222,36 @@ describe("[Case 2] Request SPECTRAL_REQUIREMENTS of TWO images and then CLOSE_FI
 
     test(`(Step 1) IMAGE 1 : OPEN_FILE_ACK and REGION_HISTOGRAM_DATA should arrive within ${openFileTimeout} ms`, async () => {
         await Connection.send(CARTA.CloseFile, { fileId: -1 });
-        await Connection.send(CARTA.CloseFile, { fileId: 0 });
-        await Connection.send(CARTA.OpenFile, assertItem.fileOpen[0]);
-        let OpenAck = await Connection.receive(CARTA.OpenFileAck)
-        await Connection.receive(CARTA.RegionHistogramData) // OpenFileAck | RegionHistogramData
-        expect(OpenAck.success).toBe(true)
-        expect(OpenAck.fileInfo.name).toEqual(assertItem.fileOpen[0].file)
+        let OpenAck = await Connection.openFile(assertItem.openFile[0]);
+        expect(OpenAck.OpenFileAck.success).toBe(true)
+        expect(OpenAck.OpenFileAck.fileInfo.name).toEqual(assertItem.openFile[0].file)
     }, openFileTimeout);
 
     let ack: AckStream;
     test(`(Step 2) IMAGE 1 : return RASTER_TILE_DATA(Stream) and check total length `, async () => {
-        await Connection.send(CARTA.AddRequiredTiles, assertItem.addTilesReq[0]);
+        await Connection.send(CARTA.AddRequiredTiles, assertItem.addRequiredTiles[0]);
         await Connection.send(CARTA.SetCursor, assertItem.setCursor[0]);
         await Connection.send(CARTA.SetSpatialRequirements, assertItem.setSpatialReq[0]);
-        ack = await Connection.stream(5, 2500) as AckStream;
+        let ack = await Connection.streamUntil((type, data) => type == CARTA.RasterTileSync ? data.endSync : false);
         expect(ack.RasterTileSync.length).toEqual(2) //RasterTileSync: start & end
-        expect(ack.RasterTileData.length).toEqual(assertItem.addTilesReq[0].tiles.length) //only 1 Tile returned
+        expect(ack.RasterTileData.length).toEqual(assertItem.addRequiredTiles[0].tiles.length) //only 1 Tile returned
     }, readFileTimeout);
 
     test(`(Step 3) IMAGE 2 : OPEN_FILE_ACK and REGION_HISTOGRAM_DATA should arrive within ${openFileTimeout} ms`, async () => {
         await Connection.send(CARTA.CloseFile, { fileId: 1 });
-        await Connection.send(CARTA.OpenFile, assertItem.fileOpen[1]);
-        let OpenAck = await Connection.receive(CARTA.OpenFileAck)
-        await Connection.receive(CARTA.RegionHistogramData) // OpenFileAck | RegionHistogramData
-        expect(OpenAck.success).toBe(true)
-        expect(OpenAck.fileInfo.name).toEqual(assertItem.fileOpen[1].file)
+        let OpenAck = await Connection.openFile(assertItem.openFile[1]);
+        expect(OpenAck.OpenFileAck.success).toBe(true)
+        expect(OpenAck.OpenFileAck.fileInfo.name).toEqual(assertItem.openFile[1].file)
     }, openFileTimeout);
 
     let ack2: AckStream;
     test(`(Step 4) IMAGE 2 : return RASTER_TILE_DATA(Stream) and check total length `, async () => {
-        await Connection.send(CARTA.AddRequiredTiles, assertItem.addTilesReq[1]);
+        await Connection.send(CARTA.AddRequiredTiles, assertItem.addRequiredTiles[1]);
         await Connection.send(CARTA.SetCursor, assertItem.setCursor[1]);
         await Connection.send(CARTA.SetSpatialRequirements, assertItem.setSpatialReq[1]);
-        ack2 = await Connection.stream(5, 2500) as AckStream;
+        let ack2 = await Connection.streamUntil((type, data) => type == CARTA.RasterTileSync ? data.endSync : false);
         expect(ack2.RasterTileSync.length).toEqual(2) //RasterTileSync: start & end
-        expect(ack2.RasterTileData.length).toEqual(assertItem.addTilesReq[1].tiles.length) //only 1 Tile returned
+        expect(ack2.RasterTileData.length).toEqual(assertItem.addRequiredTiles[1].tiles.length) //only 1 Tile returned
     }, readFileTimeout);
 
     let SetRegionAckTemp: CARTA.SetRegionAck;
@@ -269,37 +259,32 @@ describe("[Case 2] Request SPECTRAL_REQUIREMENTS of TWO images and then CLOSE_FI
     let ReceiveProgress1: number;
     let SpectralProfileDataTemp2: CARTA.SpectralProfileData;
     let ReceiveProgress2: number;
-    // let SetRegionAckTemp2: CARTA.SetRegionAck;
     test(`(Step 5) Set REGION & SPECTRAL_PROFILE streaming, once progress1>0.5 -> progress2>0.5 -> CLOSE_FILE two images & Check whether the backend is alive:`, async () => {
         // Set REGION
         await Connection.send(CARTA.SetRegion, assertItem.setRegion[0]);
         SetRegionAckTemp = await Connection.receive(CARTA.SetRegionAck)
-        // console.log(SetRegionAckTemp)
 
         //Set 1st image SPECTRAL_PROFILE streaming
         await Connection.send(CARTA.SetSpectralRequirements, assertItem.setSpectralRequirements[0]);
         SpectralProfileDataTemp1 = await Connection.receive(CARTA.SpectralProfileData);
         ReceiveProgress1 = SpectralProfileDataTemp1.progress;
-        // console.log(SpectralProfileDataTemp1)
         if (ReceiveProgress1 != 1) {
             while (ReceiveProgress1 <= 0.5) {
                 SpectralProfileDataTemp1 = await Connection.receive(CARTA.SpectralProfileData);
                 ReceiveProgress1 = SpectralProfileDataTemp1.progress
-                console.warn('(Case 2) ' + assertItem.fileOpen[0].file + ' SPECTRAL_PROFILE progress :', ReceiveProgress1)
+                console.warn('(Case 2) ' + assertItem.openFile[0].file + ' SPECTRAL_PROFILE progress :', ReceiveProgress1)
             };
             await Connection.send(CARTA.SetSpectralRequirements, assertItem.setSpectralRequirements[1]);
 
             // Set  2nd image SPECTRAL_PROFILE streaming
             await Connection.send(CARTA.SetSpectralRequirements, assertItem.setSpectralRequirements[2]);
             SpectralProfileDataTemp2 = await Connection.receive(CARTA.SpectralProfileData);
-            console.log(SpectralProfileDataTemp2)
             ReceiveProgress2 = SpectralProfileDataTemp2.progress;
             if (ReceiveProgress2 != 1) {
                 while (ReceiveProgress2! < 0.5) {
                     SpectralProfileDataTemp2 = await Connection.receive(CARTA.SpectralProfileData);
-                    // console.log(SpectralProfileDataTemp2)
                     ReceiveProgress2 = SpectralProfileDataTemp2.progress
-                    console.warn('(Case 2) ' + assertItem.fileOpen[1].file + ' SPECTRAL_PROFILE progress :', ReceiveProgress2)
+                    console.warn('(Case 2) ' + assertItem.openFile[1].file + ' SPECTRAL_PROFILE progress :', ReceiveProgress2)
                 };
                 //Once ReceiveProgress2>0.5, then CLOSE_FILE to 1st & 2nd image
                 await Connection.send(CARTA.CloseFile, { fileId: 0 });
