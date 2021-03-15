f826edfb0dc5c0e7ac50d40472fe5ab59a42a877
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2021-03-15 10:53:52 +0800

    Add argument to restart docker if failover

--- Docker/build_run.sh
+++ Docker/build_run.sh
@@ -3,7 +3,7 @@ IMAGE_NAME="carta-backend-dev"
 docker build -f Dockerfile-carta_U20.04 -t $IMAGE_NAME .
 
 echo $IMAGE_NAME is running
-CONTAINER_ID=$(docker run -it -p 3002:3002 -v ~/CARTA/Images/:/Images $IMAGE_NAME /Images --verbosity 6 --no_log --no_http --no_browser --debug_no_auth)
+CONTAINER_ID=$(docker run --restart=on-failure -it -p 3002:3002 -v ~/CARTA/Images/:/Images $IMAGE_NAME /Images --verbosity 6 --no_log --no_http --no_browser --debug_no_auth)
 # docker cp build.sh $CONTAINER_ID:/build.sh
 # docker exec -t $CONTAINER_ID /bin/sh -c '/bin/sh /build.sh'
 # echo $CONTAINER_ID
