a3b696ba10b368f493c8f38aa06e22f1bd668149
Ming-yi Lin <acdo2002@gmail.com>
2021-03-17 17:52:44 +0800

    add new test: FILEINFO_HDF5_COMPARE_FITS.test.ts

--- src/test/FILEINFO_HDF5_COMPARE_FITS.test.ts
+++ src/test/FILEINFO_HDF5_COMPARE_FITS.test.ts
@@ -2,8 +2,9 @@ import { CARTA } from "carta-protobuf";
 
 import { Client } from "./CLIENT";
 import config from "./config.json";
+const WebSocket = require('isomorphic-ws');
 
-let testServerUrl = config.serverURL;
+let testServerUrl = config.serverURL0;
 let testSubdirectory = config.path.QA;
 let connectTimeout = config.timeout.connection;
 let listFileTimeout = config.timeout.listFile;
@@ -22,12 +23,87 @@ let assertItem: AssertItem = {
     fileInfoRequest: 
     [
         {
-            file: "M17_SWex.hdf5",
-            hdu: "",
-        },
-        {
             file: "M17_SWex.fits",
             hdu: "",
         },
+        {
+            file: "M17_SWex.hdf5",
+            hdu: "",
+        },
     ]
-}
+};
+
+describe("FILEINFO_HDF5: Testing if info of an HDF5 image file is correctly delivered by the backend", () => {
+
+    let Connection: Client;
+    beforeAll(async () => {
+        Connection = new Client(testServerUrl);
+        await Connection.open();
+        await Connection.registerViewer(assertItem.registerViewer);
+    }, connectTimeout);
+    
+
+    describe(`Go to "${testSubdirectory}" folder`, () => {
+        let basePath: string;
+        beforeAll(async () => {
+            await Connection.send(CARTA.FileListRequest, { directory: "$BASE" });
+            basePath = (await Connection.receive(CARTA.FileListResponse) as CARTA.FileListResponse).directory;
+        }, listFileTimeout);
+
+        test(`(Step 0) Connection open? | `, () => {
+            expect(Connection.connection.readyState).toBe(WebSocket.OPEN);
+        });
+
+        describe(`Open hdf5 then open fits, then compare`,()=>{
+
+            let FileInfo_fits_Response: CARTA.FileInfoResponse;
+            let fitsHeaderEntries: any;
+            test(`Open fits`,async()=>{
+                await Connection.send(CARTA.FileInfoRequest, {
+                    directory: `${basePath}/` + testSubdirectory,
+                    ...assertItem.fileInfoRequest[0],
+                });
+                FileInfo_fits_Response = await Connection.receive(CARTA.FileInfoResponse);
+                fitsHeaderEntries =  FileInfo_fits_Response.fileInfoExtended['0'].headerEntries//.fileInfoExtended['0'].headerEntries);
+            });
+
+            let FileInfo_hdf5_Response: CARTA.FileInfoResponse;
+            let hdf5HeaderEntries: any;
+            test(`Open hdf5`,async()=>{
+                await Connection.send(CARTA.FileInfoRequest, {
+                    directory: `${basePath}/` + testSubdirectory,
+                    ...assertItem.fileInfoRequest[1],
+                });
+                FileInfo_hdf5_Response = await Connection.receive(CARTA.FileInfoResponse);
+                hdf5HeaderEntries = FileInfo_hdf5_Response.fileInfoExtended['0'].headerEntries//.fileInfoExtended['0'].headerEntries);
+            });
+
+            test(`Match each header entry of hdf5 to fits`,()=>{
+                let hdf5HeaderArray = [];
+                hdf5HeaderEntries.map((hdf5entry,index)=>{
+                    hdf5HeaderArray.push(hdf5entry.name);
+                });
+
+                let fitsHeaderArray = [];
+                fitsHeaderEntries.map((fitsentry,index)=>{
+                    fitsHeaderArray.push(fitsentry.name);
+                });
+
+                let intersection = hdf5HeaderArray.filter(x => fitsHeaderArray.includes(x));
+
+                let difference = hdf5HeaderArray.filter(x => !fitsHeaderArray.includes(x));
+                console.warn("Additional header entries in hdf5:",difference)
+
+                intersection.map((input,index)=>{
+                   let hdf5Index = hdf5HeaderEntries.findIndex(x => x.name == input);
+                   let fitsIndex = fitsHeaderEntries.findIndex(y => y.name == input);
+                   expect(hdf5HeaderEntries[hdf5Index].value).toEqual(fitsHeaderEntries[fitsIndex].value)
+                //    console.log(hdf5HeaderEntries[hdf5Index].value);
+                //    console.log(fitsHeaderEntries[fitsIndex].value);
+                });
+            });
+        });
+    
+    });
+    afterAll(() => Connection.close());
+});
