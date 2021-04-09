e81b03cf4f8dbb7cddbc7339cb30e07492ed4d93
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2021-04-09 14:32:07 +0800

    Add test FILEINFO_CASA_VARIANT with snapshot

---
+++ src/test/FILEINFO_CASA_VARIANT.test.ts
@@ -0,0 +1,76 @@
+import { CARTA } from "carta-protobuf";
+
+import { Client } from "./CLIENT";
+import config from "./config.json";
+
+let testServerUrl = config.serverURL;
+let testSubdirectory = '/set_casaImageVarients';
+let connectTimeout = config.timeout.connection;
+let listFileTimeout = config.timeout.listFile;
+let openFileTimeout = config.timeout.openFile;
+
+interface AssertItem {
+    registerViewer: CARTA.IRegisterViewer;
+    fileInfoRequest: CARTA.IFileInfoRequest[];
+};
+
+let assertItem: AssertItem = {
+    registerViewer: {
+        sessionId: 0,
+        clientFeatureFlags: 5,
+    },
+    fileInfoRequest: [
+        {
+            file: "componentlist.image",
+            hdu: "",
+        },
+        {
+            file: "concatenated.image",
+            hdu: "",
+        },
+        {
+            file: "pVimage.image",
+            hdu: "",
+        },
+        {
+            file: "UVamp.image",
+            hdu: "",
+        },
+        {
+            file: "UVphase.image",
+            hdu: "",
+        },
+    ],
+};
+
+describe("FILEINFO_CASA_VARIANT: Testing if file info of a variant CASA images is correctly delivered by the backend", () => {
+
+    let Connection: Client;
+    beforeAll(async () => {
+        Connection = new Client(testServerUrl);
+        await Connection.open();
+        await Connection.registerViewer(assertItem.registerViewer);
+    }, connectTimeout);
+
+    describe(`Go to "${testSubdirectory}" folder`, () => {
+        let basePath: string;
+        beforeAll(async () => {
+            await Connection.send(CARTA.FileListRequest, { directory: "$BASE" });
+            basePath = (await Connection.receive(CARTA.FileListResponse) as CARTA.FileListResponse).directory;
+        }, listFileTimeout);
+        assertItem.fileInfoRequest.map(fileInfoRequest=>{
+            describe(`query the info of file : ${fileInfoRequest.file}`, () => {
+                let FileInfoResponse: CARTA.FileInfoResponse;
+                test(`FILE_INFO_RESPONSE should arrive within ${openFileTimeout} ms".`, async () => {
+                    await Connection.send(CARTA.FileInfoRequest, {
+                        directory: `${basePath}/` + testSubdirectory,
+                        ...fileInfoRequest,
+                    });
+                    FileInfoResponse = await Connection.receive(CARTA.FileInfoResponse);
+                    expect(FileInfoResponse).toMatchSnapshot();
+                }, openFileTimeout);
+            });
+        });
+    });
+    afterAll(() => Connection.close());
+});
