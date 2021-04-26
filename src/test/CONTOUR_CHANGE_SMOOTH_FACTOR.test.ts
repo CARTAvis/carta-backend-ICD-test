f13ae0de3d220285fb8464ca3a8399be4d232331
Ming-yi Lin <acdo2002@gmail.com>
2021-04-26 14:41:13 +0800

    change testServerUrl to demo server

--- src/test/CONTOUR_CHANGE_SMOOTH_FACTOR.test.ts
+++ src/test/CONTOUR_CHANGE_SMOOTH_FACTOR.test.ts
@@ -5,7 +5,7 @@ import config from "./config.json";
 const WebSocket = require('isomorphic-ws');
 const ZstdCodec = require('zstd-codec').ZstdCodec;
 
-let testServerUrl: string = config.serverURL0;
+let testServerUrl: string = config.serverURL;
 let testSubdirectory: string = config.path.QA;
 let connectTimeout: number = config.timeout.connection;
 let openFileTimeout: number = config.timeout.openFile;
