5f0a80b2d869292e316fe428ea21cbe196c6709d
Ming-yi Lin <acdo2002@gmail.com>
2021-05-06 17:43:04 +0800

    finish case 1 of CONCAT_ERROR_MESSAGE.test.ts

--- src/test/CONCAT_ERROR_MESSAGE.test.ts
+++ src/test/CONCAT_ERROR_MESSAGE.test.ts
@@ -9,17 +9,13 @@ let connectTimeout: number = config.timeout.connection;
 let openFileTimeout = config.timeout.openFile;
 let concatStokeTimeout = config.timeout.concatStokes;
 
-interface ConcatStokesFilesAckExt extends CARTA.IConcatStokesFilesAck {
-    OpenFileAckBeamLength: number
-}
 
 interface AssertItem {
     register: CARTA.IRegisterViewer;
     filelist: CARTA.IFileListRequest;
     fileOpen: CARTA.IOpenFile;
-    fileInfoReq: CARTA.IFileInfoRequest[];
     ConcatReq: CARTA.IConcatStokesFiles;
-    ConcatResponse: ConcatStokesFilesAckExt;
+    ConcatResponse: string[];
 };
 
 let assertItem: AssertItem = {
@@ -28,24 +24,6 @@ let assertItem: AssertItem = {
         clientFeatureFlags: 5,
     },
     filelist: { directory: "$BASE" },
-    fileInfoReq:[
-        {
-            file:"IRCp10216_sci.spw0.cube.I.manual.pbcor.fits",
-            hdu: "",
-        },
-        {
-            file:"IRCp10216_sci.spw0.cube.Q.manual.pbcor.fits",
-            hdu: "",
-        },
-        {
-            file:"IRCp10216_sci.spw0.cube.U.manual.pbcor.fits",
-            hdu: "",
-        },
-        {
-            file:"IRCp10216_sci.spw0.cube.V.manual.pbcor.fits",
-            hdu: "",
-        },
-    ],
     ConcatReq:{
         fileId: 0,
         renderMode: 0,
@@ -62,37 +40,12 @@ let assertItem: AssertItem = {
                 file:'IRCp10216_sci.spw0.cube.U.manual.pbcor.fits',
                 stokesType: 3
             },
-            {
-                directory: testSubdirectory, 
-                hdu:"",
-                file:'IRCp10216_sci.spw0.cube.Q.manual.pbcor.fits',
-                stokesType: 2
-            },
-            {
-                directory: testSubdirectory, 
-                hdu:"",
-                file:'IRCp10216_sci.spw0.cube.I.manual.pbcor.fits',
-                stokesType: 1
-            },
         ],
     },
-    ConcatResponse:{
-        success: true,
-        openFileAck: {
-            success: true,
-            fileInfo: {
-                name:"IRCp10216_sci.spw0.cube.hypercube_IQUV.manual.pbcor.fits"
-            },
-            fileInfoExtended:{
-                depth: 480,
-                dimensions: 4,
-                height: 256,
-                stokes: 4,
-                width: 256,
-            },
-        },
-        OpenFileAckBeamLength: 1920,
-    }
+    ConcatResponse:[
+        'is not allowed!',
+        'are not consistent!'
+    ]
 };
 
 describe("CONCAT_STOKES_IMAGES test: concatenate different stokes images into single image", () => {
@@ -106,37 +59,27 @@ describe("CONCAT_STOKES_IMAGES test: concatenate different stokes images into si
     
     let basePath: string;
     describe(`Go to "${testSubdirectory}" folder`,()=>{
-        test(`(Step 1) Assert FileListRequest |`, async()=>{
-            await Connection.send(CARTA.FileListRequest,assertItem.filelist);
-            basePath = (await Connection.receive(CARTA.FileListResponse) as CARTA.FileListResponse).directory;
-        });
-
-        let FileInfoResponse: CARTA.FileInfoResponse;
-        assertItem.fileInfoReq.map((input,index)=>{
-            test(`FILE_INFO_RESPONSE-${index+1} should arrive within ${openFileTimeout} ms" | `, async () => {
-                await Connection.send(CARTA.FileInfoRequest, {
-                    directory: `${basePath}/` + testSubdirectory,
-                    ...input,
-                });
-                FileInfoResponse = await Connection.receive(CARTA.FileInfoResponse);
-                expect(FileInfoResponse.success).toEqual(true);
-            }, openFileTimeout);
-        });
-
-        let ConcatStokesResponse: CARTA.ConcatStokesFilesAck;
-        test(`(Step 2) Modify assert concatenate directory and request CONCAT_STOKES_FILES_ACK within ${concatStokeTimeout} ms | `,async()=>{
-            assertItem.ConcatReq.stokesFiles.map((input,index)=>{
-                assertItem.ConcatReq.stokesFiles[index].directory = basePath + `/` + testSubdirectory; //`${basePath}/` + testSubdirectory;
+        describe(`Case 1: U & V`,()=>{
+            test(`(Step 1) Assert FileListRequest |`, async()=>{
+                await Connection.send(CARTA.FileListRequest,assertItem.filelist);
+                basePath = (await Connection.receive(CARTA.FileListResponse) as CARTA.FileListResponse).directory;
             });
 
-            await Connection.send(CARTA.CloseFile, { fileId: -1 });
-            await Connection.send(CARTA.ConcatStokesFiles,assertItem.ConcatReq);
-            await Connection.receive(CARTA.RegionHistogramData);
-            ConcatStokesResponse = await Connection.receive(CARTA.ConcatStokesFilesAck);
-            // console.log(ConcatStokesResponse);
-            
-        },concatStokeTimeout);
-});
+            let ConcatStokesResponse: CARTA.ConcatStokesFilesAck;
+            test(`(Step 2) Modify assert concatenate directory and request CONCAT_STOKES_FILES_ACK within ${concatStokeTimeout} ms | `,async()=>{
+                assertItem.ConcatReq.stokesFiles.map((input,index)=>{
+                    assertItem.ConcatReq.stokesFiles[index].directory = basePath + `/` + testSubdirectory; //`${basePath}/` + testSubdirectory;
+                });
 
+                await Connection.send(CARTA.CloseFile, { fileId: -1 });
+                await Connection.send(CARTA.ConcatStokesFiles,assertItem.ConcatReq);
+                ConcatStokesResponse = await Connection.receive(CARTA.ConcatStokesFilesAck);
+                // console.log(ConcatStokesResponse.message);
+                // console.log(assertItem.ConcatResponse[0])
+                expect(ConcatStokesResponse.message).toContain(assertItem.ConcatResponse[0]);
+            
+            },concatStokeTimeout);
+        });
+    });
     afterAll(() => Connection.close());
 });
