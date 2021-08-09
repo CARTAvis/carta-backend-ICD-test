81234625f07466209152cb1f15c1dd90697216a8
Ming-Yi Lin <acdo2002@gmail.com>
2021-08-09 23:04:40 +0800

    update PER_CUBE_HISTOGRAM_HDF5.test.ts

--- src/test/PER_CUBE_HISTOGRAM_HDF5.test.ts
+++ src/test/PER_CUBE_HISTOGRAM_HDF5.test.ts
@@ -8,7 +8,7 @@ let testSubdirectory = config.path.QA;
 let connectTimeout = config.timeout.connection;
 let openFileTimeout = config.timeout.openFile;
 let readFileTimeout = config.timeout.readFile;
-let regionTimeout = config.timeout.region;
+let cubeHistogramTimeout = config.timeout.cubeHistogram;
 
 interface IRegionHistogramDataExt extends CARTA.IRegionHistogramData {
     lengthOfHistogramBins: number;
@@ -56,14 +56,13 @@ let assertItem: AssertItem = {
     },
     regionHistogramData: {
         regionId: -2,
-        histograms: [
-            {
-                channel: -2,
-                numBins: 2775,
-                binWidth: 0.7235205573004645,
-                firstBinCenter: -1773.2998608150997,
-            },
-        ],
+        channel: -2,
+        histograms: 
+        {
+            numBins: 2775,
+            binWidth: 0.7235205573004645,
+            firstBinCenter: -1773.2998046875,
+        },
         lengthOfHistogramBins: 2775,
         binValues: [{ index: 2500, value: 9359604 },],
         mean: 18.742310241547514,//18.742310241547514 for socketdev; 18.743059611332498 for socketicd
@@ -95,43 +94,43 @@ describe("PER_CUBE_HISTOGRAM_HDF5: Testing calculations of the per-cube histogra
     describe(`Request histogram requirements:`, () => {
         let ReceiveProgress: number;
         let RegionHistogramData: CARTA.RegionHistogramData;
-        test(`REGION_HISTOGRAM_DATA should arrive completely within ${regionTimeout} ms:`, async () => {
+        test(`REGION_HISTOGRAM_DATA should arrive completely within ${cubeHistogramTimeout} ms:`, async () => {
             await Connection.send(CARTA.SetHistogramRequirements, assertItem.setHistogramRequirements);
-            let ack = await Connection.streamUntil((type, data) => type == CARTA.RegionHistogramData && data.progress > 0.5);
+            let ack = await Connection.streamUntil((type, data) => type == CARTA.RegionHistogramData && data.progress == 1.0);
             RegionHistogramData = ack.RegionHistogramData.slice(-1)[0];
             ReceiveProgress = RegionHistogramData.progress;
-        }, regionTimeout);
+        }, cubeHistogramTimeout);
 
         test(`Assert REGION_HISTOGRAM_DATA.progress == 1`, () => {
             expect(ReceiveProgress).toBe(1);
         });
 
-        test(`Assert REGION_HISTOGRAM_DATA.histograms.bin_width = ${assertItem.regionHistogramData.histograms[0].binWidth}`, () => {
-            expect(RegionHistogramData.histograms[0].binWidth).toBeCloseTo(assertItem.regionHistogramData.histograms[0].binWidth, assertItem.precisionDigits);
+        test(`Assert REGION_HISTOGRAM_DATA.histograms.bin_width = ${assertItem.regionHistogramData.histograms.binWidth}`, () => {
+            expect(RegionHistogramData.histograms.binWidth).toBeCloseTo(assertItem.regionHistogramData.histograms.binWidth, assertItem.precisionDigits);
         });
 
         test(`Assert len(REGION_HISTOGRAM_DATA.histograms.bins) = ${assertItem.regionHistogramData.lengthOfHistogramBins}`, () => {
-            expect(RegionHistogramData.histograms[0].bins.length).toEqual(assertItem.regionHistogramData.lengthOfHistogramBins);
+            expect(RegionHistogramData.histograms.bins.length).toEqual(assertItem.regionHistogramData.lengthOfHistogramBins);
         });
 
         test(`Assert REGION_HISTOGRAM_DATA.histograms.bins[2500] = 9359604`, () => {
-            expect(RegionHistogramData.histograms[0].bins[2500]).toEqual(assertItem.regionHistogramData.binValues[0].value);
+            expect(RegionHistogramData.histograms.bins[2500]).toEqual(assertItem.regionHistogramData.binValues[0].value);
         });
 
-        test(`Assert REGION_HISTOGRAM_DATA.histograms.firt_bin_center = ${assertItem.regionHistogramData.histograms[0].firstBinCenter}`, () => {
-            expect(RegionHistogramData.histograms[0].firstBinCenter).toBeCloseTo(assertItem.regionHistogramData.histograms[0].firstBinCenter, assertItem.precisionDigits);
+        test(`Assert REGION_HISTOGRAM_DATA.histograms.firt_bin_center = ${assertItem.regionHistogramData.histograms.firstBinCenter}`, () => {
+            expect(RegionHistogramData.histograms.firstBinCenter).toBeCloseTo(assertItem.regionHistogramData.histograms.firstBinCenter, assertItem.precisionDigits);
         });
 
-        test(`Assert REGION_HISTOGRAM_DATA.histograms.num_bins = ${assertItem.regionHistogramData.histograms[0].numBins}`, () => {
-            expect(RegionHistogramData.histograms[0].numBins).toEqual(assertItem.regionHistogramData.histograms[0].numBins);
+        test(`Assert REGION_HISTOGRAM_DATA.histograms.num_bins = ${assertItem.regionHistogramData.histograms.numBins}`, () => {
+            expect(RegionHistogramData.histograms.numBins).toEqual(assertItem.regionHistogramData.histograms.numBins);
         });
 
         test(`Assert REGION_HISTOGRAM_DATA.histograms.mean = ${assertItem.regionHistogramData.mean}`, () => {
-            expect(RegionHistogramData.histograms[0].mean).toBeCloseTo(assertItem.regionHistogramData.mean, assertItem.precisionDigits)
+            expect(RegionHistogramData.histograms.mean).toBeCloseTo(assertItem.regionHistogramData.mean, assertItem.precisionDigits)
         });
 
         test(`Assert REGION_HISTOGRAM_DATA.histograms.stdDev = ${assertItem.regionHistogramData.stdDev}`, () => {
-            expect(RegionHistogramData.histograms[0].stdDev).toBeCloseTo(assertItem.regionHistogramData.stdDev, assertItem.precisionDigits)
+            expect(RegionHistogramData.histograms.stdDev).toBeCloseTo(assertItem.regionHistogramData.stdDev, assertItem.precisionDigits)
         });
 
         test(`Assert REGION_HISTOGRAM_DATA.histograms.region_id = ${assertItem.regionHistogramData.regionId}`, () => {
