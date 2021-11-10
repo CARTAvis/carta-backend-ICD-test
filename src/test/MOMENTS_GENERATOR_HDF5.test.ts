0e95af16db7fe653053e563c0426b3f73005010c
Ming-yi Lin <acdo2002@gmail.com>
2021-11-10 16:05:31 +0800

    finish MOMENTS_GENERATOR_HDF5.test.ts

--- src/test/MOMENTS_GENERATOR_HDF5.test.ts
+++ src/test/MOMENTS_GENERATOR_HDF5.test.ts
@@ -146,9 +146,9 @@ describe("MOMENTS_GENERATOR_HDF5: Testing moments generator for a given region o
             });
         });
 
-        test(`Assert openFileAcks[].fileInfoExtended.headerEntries.length = 78`, () => {
+        test(`Assert openFileAcks[].fileInfoExtended.headerEntries.length = 79`, () => {
             ack.MomentResponse[0].openFileAcks.map((ack, index) => {
-                expect(ack.fileInfoExtended.headerEntries.length).toEqual(78);
+                expect(ack.fileInfoExtended.headerEntries.length).toEqual(79);
             });
         });
 
