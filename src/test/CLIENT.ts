88cfeff600aef316ad94a7ed7a67b25e9d49bcca
ming-yi <ming-yi@ming-yideMacBook-Pro.local>
2022-02-23 11:25:43 +0800

    update CLIENT.ts

--- src/test/CLIENT.ts
+++ src/test/CLIENT.ts
@@ -9,7 +9,7 @@ export interface IOpenFile {
     RegionHistogramData: CARTA.RegionHistogramData;
 }
 export class Client {
-    IcdVersion: number = 25;
+    IcdVersion: number = 26;
     CartaType = new Map<number, any>([
         [0, CARTA.ErrorData],
         [1, CARTA.RegisterViewer],
