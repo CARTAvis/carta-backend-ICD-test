471dc7a3820effd7f147e4401a9dd74bfeb33f0d
Ming-yi Lin <acdo2002@gmail.com>
2022-03-31 17:30:13 +0800

    Add ACCESS_BACKEND.test.ts

---
+++ src/test-new/ACCESS_BACKEND.test.ts
@@ -0,0 +1,18 @@
+import {MessageController, ConnectionStatus} from "./MessageController";
+import config from "./config.json";
+let testServerUrl = config.serverURL0;
+let connectTimeout = config.timeout.connection;
+
+describe("GET_FILELIST_DEFAULT_PATH tests: Testing generation of a file list at default path ($BASE)", () => {
+
+    const msgController = MessageController.Instance;
+    beforeAll(async ()=> {
+        await msgController.connect(testServerUrl);
+    }, connectTimeout);
+    
+    test(`Connect to the backend`, async()=>{
+        expect(msgController.connectionStatus).toBe(ConnectionStatus.ACTIVE);
+    })
+
+    afterAll(() => msgController.closeConnection());
+});
