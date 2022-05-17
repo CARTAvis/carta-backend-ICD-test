81eb5222d834e5bd55a9a94a513f4334b10888e2
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2022-05-17 11:44:26 +0800

    increase histogram and pv generator timeouts

--- src/test/PER_CUBE_HISTOGRAM_CANCELLATION.test.ts
+++ src/test/PER_CUBE_HISTOGRAM_CANCELLATION.test.ts
@@ -112,11 +112,11 @@ describe("PER_CUBE_HISTOGRAM_CANCELLATION: Testing calculations of the per-cube 
             let ReceiveProgress: number;
             let RegionHistogramData: CARTA.RegionHistogramData;
             describe(`Set histogram requirements:`, () => {
-                test(`(Step1) "${assertItem.openFile.file}" REGION_HISTOGRAM_DATA should arrive completely within 3000 ms:`, async () => {
+                test(`(Step1) "${assertItem.openFile.file}" REGION_HISTOGRAM_DATA should arrive completely within 6000 ms:`, async () => {
                     await Connection.send(CARTA.SetHistogramRequirements, assertItem.setHistogramRequirements);
                     RegionHistogramData = await Connection.receive(CARTA.RegionHistogramData);
                     ReceiveProgress = RegionHistogramData.progress;
-                }, 3000);
+                }, 6000);
 
                 test(`(Step2) REGION_HISTOGRAM_DATA.progress > 0 and REGION_HISTOGRAM_DATA.region_id = ${assertItem.regionHistogramData.regionId}`, () => {
                     expect(ReceiveProgress).toBeGreaterThan(0);
