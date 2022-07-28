2de3dd16603482bb0a4d90a8ef9599877ea3293f
ming-yi <ming-yi@ming-yideMacBook-Pro.local>
2022-07-28 13:43:02 +0800

    finish IMAGE_FITTING_FITS.test.ts

--- src/test/IMAGE_FITTING_FITS.test.ts
+++ src/test/IMAGE_FITTING_FITS.test.ts
@@ -6,7 +6,7 @@ import { Client, AckStream } from "./CLIENT";
 import config from "./config.json";
 const WebSocket = require('isomorphic-ws');
 
-let testServerUrl: string = config.serverURL0;
+let testServerUrl: string = config.serverURL;
 let testSubdirectory: string = config.path.QA;
 let connectTimeout: number = config.timeout.connection;
 let openFileTimeout: number = config.timeout.openFile;
@@ -53,6 +53,17 @@ let assertItem: AssertItem = {
             fovInfo: null,
             regionId: -1, 
             initialValues: [{amp: 10, center: {x: 320, y: 400}, fwhm: {x: 100, y: 50}, pa: 135}]
+        },
+        {
+            fileId: 0,
+            fixedParams: [],
+            fovInfo: {
+                controlPoints: [{x:319.5, y:399.5}, {x: 216.70644391408112, y: 199.99999999999997}],
+                regionType: 3,
+                rotation: 0,
+            },
+            regionId: 0, 
+            initialValues: [{amp: 10, center: {x: 320, y: 400}, fwhm: {x: 100, y: 50}, pa: 135}]
         }
     ],
     fittingResponse: [
@@ -71,6 +82,26 @@ let assertItem: AssertItem = {
                     amp: 0.00003971187313364324,
                     fwhm: {x: 0.0008773295469374465, y: 0.00021417449809861666},
                     pa: 0.00020655506565535857
+                }
+            ],
+            success: true,
+            log: 'Gaussian fitting with 1 component'
+        },
+        {
+            resultValues: [
+                {
+                    center: {x: 319.498940837943, y: 399.4988615251924}, 
+                    amp: 9.999454722592997,
+                    fwhm: {x: 170.6382683676851, y: 41.48206117869241},
+                    pa: 142.16251357416306
+                }
+            ],
+            resultErrors: [
+                {
+                    center: {x: 0.00010950493723812066, y: 0.0001246435132691238},
+                    amp: 0.000020566430029630594,
+                    fwhm: {x: 0.0004562033522657872, y: 0.00010502008093686605},
+                    pa: 0.00010562266856995906
                 }
             ],
             success: true,
@@ -115,7 +146,7 @@ describe("IMAGE_FITTING_FITS test: Testing Image Fitting (with and without fov) 
 
         });
 
-        describe(`(Case 1) Image fitting withou FoV:`, ()=>{
+        describe(`(Case 1) Image fitting without FoV:`, ()=>{
             test(`Send Image fitting request and match the result`, async()=>{
                 await Connection.send(CARTA.FittingRequest, assertItem.fittingRequest[0]);
                 let response = await Connection.receive(CARTA.FittingResponse);
@@ -137,6 +168,27 @@ describe("IMAGE_FITTING_FITS test: Testing Image Fitting (with and without fov) 
             },imageFittingTimeout)
         })
 
+        describe(`(Case 2) Image fitting with FoV:`, ()=>{
+            test(`Send Image fitting request and match the result`, async()=>{
+                await Connection.send(CARTA.FittingRequest, assertItem.fittingRequest[1]);
+                let response = await Connection.receive(CARTA.FittingResponse);
+                
+                expect(response.resultValues[0].center.x).toBeCloseTo(assertItem.fittingResponse[1].resultValues[0].center.x, assertItem.precisionDigits);
+                expect(response.resultValues[0].center.y).toBeCloseTo(assertItem.fittingResponse[1].resultValues[0].center.y, assertItem.precisionDigits);
+                expect(response.resultValues[0].amp).toBeCloseTo(assertItem.fittingResponse[1].resultValues[0].amp, assertItem.precisionDigits);
+                expect(response.resultValues[0].fwhm.x).toBeCloseTo(assertItem.fittingResponse[1].resultValues[0].fwhm.x, assertItem.precisionDigits);
+                expect(response.resultValues[0].fwhm.y).toBeCloseTo(assertItem.fittingResponse[1].resultValues[0].fwhm.y, assertItem.precisionDigits);
+                expect(response.resultValues[0].pa).toBeCloseTo(assertItem.fittingResponse[1].resultValues[0].pa, assertItem.precisionDigits);
+                expect(response.success).toEqual(assertItem.fittingResponse[1].success);
+                expect(response.resultErrors[0].center.x).toBeCloseTo(assertItem.fittingResponse[1].resultErrors[0].center.x, assertItem.precisionDigits);
+                expect(response.resultErrors[0].center.y).toBeCloseTo(assertItem.fittingResponse[1].resultErrors[0].center.y, assertItem.precisionDigits);
+                expect(response.resultErrors[0].amp).toBeCloseTo(assertItem.fittingResponse[1].resultErrors[0].amp, assertItem.precisionDigits);
+                expect(response.resultErrors[0].fwhm.x).toBeCloseTo(assertItem.fittingResponse[1].resultErrors[0].fwhm.x, assertItem.precisionDigits);
+                expect(response.resultErrors[0].fwhm.y).toBeCloseTo(assertItem.fittingResponse[1].resultErrors[0].fwhm.y, assertItem.precisionDigits);
+                expect(response.resultErrors[0].pa).toBeCloseTo(assertItem.fittingResponse[1].resultErrors[0].pa, assertItem.precisionDigits);
+                expect(response.log).toContain(assertItem.fittingResponse[1].log);
+            },imageFittingTimeout)
+        })
     });
 
     test(`close file`, async () => {
